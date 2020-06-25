0f0372aee4fdef8481bf1aaa37b40f6113c9b99c
katalyzator <aibek.raiymbekov01@gmail.com>
2020-06-25 16:54:40 +0600

    added backlash

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from users.views import RegisterAuthAPIView
 
 urlpatterns = [
-    path('register_auth', RegisterAuthAPIView.as_view(), name='register_auth')
+    path('register_auth/', RegisterAuthAPIView.as_view(), name='register_auth')
 ]

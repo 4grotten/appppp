9d9c7fb0cb857034b48e24797a69138e1cdf9abf
katalyzator <aibek.raiymbekov01@gmail.com>
2020-06-26 16:24:26 +0600

    changed static url name

--- project/settings/base.py
+++ project/settings/base.py
@@ -116,8 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/d-static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'd-static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'

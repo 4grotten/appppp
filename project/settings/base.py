e67b574084250f32319146e761703598ca0c5f99
dadaday <aaurub@gmail.com>
2020-07-15 01:39:40 +0600

    misc changes

--- project/settings/base.py
+++ project/settings/base.py
@@ -175,6 +175,5 @@ EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='login@yandex.ru')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='password')
-DEFAULT_FROM_EMAIL = 'login@example.com'
-EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='from@yandex.ru')
+EMAIL_USE_TLS = True

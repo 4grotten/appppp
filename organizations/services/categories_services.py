27ab86fff30e3740ffa193a7030df724303325de
dadaday <aaurub@gmail.com>
2020-07-27 01:31:44 +0600

    order by orgs count in categories

--- organizations/services/categories_services.py
+++ organizations/services/categories_services.py
@@ -1,4 +1,4 @@
-from django.db.models import QuerySet
+from django.db.models import QuerySet, Count
 
 from organizations.models import OrganizationCategory, Organization
 
@@ -8,8 +8,10 @@ class OrganizationCategoryService:
     def get_nonempty_categories(cls, partner: Organization = None) -> QuerySet:
         # ToDo: Exclude categories where all organizations are deactivated (Rare case)
         if partner is None:
-            return OrganizationCategory.objects.filter(types__organizations__isnull=False).distinct()
+            return OrganizationCategory.objects.filter(types__organizations__isnull=False).annotate(
+                orgs_count=Count('types__organizations')).distinct().order_by('-orgs_count')
 
         return OrganizationCategory.objects.filter(
             types__organizations__in=partner.requested_partnerships.filter(is_accepted=True).values_list(
-                'accepted_by', flat=True)).distinct()
+                'accepted_by', flat=True)).annotate(
+            orgs_count=Count('types__organizations')).distinct().order_by('-orgs_count')

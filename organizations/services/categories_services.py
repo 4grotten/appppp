3b94ff2be4d07ea73e2d322f17f4aa88748460c7
dadaday <aaurub@gmail.com>
2020-07-27 01:49:21 +0600

    order by orgs count in categories (fix distinct)

--- organizations/services/categories_services.py
+++ organizations/services/categories_services.py
@@ -9,9 +9,9 @@ class OrganizationCategoryService:
         # ToDo: Exclude categories where all organizations are deactivated (Rare case)
         if partner is None:
             return OrganizationCategory.objects.filter(types__organizations__isnull=False).annotate(
-                orgs_count=Count('types__organizations')).distinct().order_by('-orgs_count')
+                orgs_count=Count('types__organizations', distinct=True)).distinct().order_by('-orgs_count')
 
         return OrganizationCategory.objects.filter(
             types__organizations__in=partner.requested_partnerships.filter(is_accepted=True).values_list(
                 'accepted_by', flat=True)).annotate(
-            orgs_count=Count('types__organizations')).distinct().order_by('-orgs_count')
+            orgs_count=Count('types__organizations', distinct=True)).distinct().order_by('-orgs_count')

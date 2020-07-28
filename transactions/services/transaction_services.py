eb5fb187ae14cb00ae26d5ce29e0c082a2af1046
Yrysbek Sagyndykov <Sagyndykov1998@gmail.com>
2020-07-28 12:06:34 +0600

    changed updated_at to created_at

--- transactions/services/transaction_services.py
+++ transactions/services/transaction_services.py
@@ -80,14 +80,14 @@ class TransactionService:
 
     @classmethod
     def get_user_transaction_organizations(cls, client: User, start_day, end_day):
-        transactions = Transaction.objects.filter(client=client).filter(updated_at__range=[start_day, end_day])
+        transactions = Transaction.objects.filter(client=client).filter(created_at__range=[start_day, end_day])
         organizations = Organization.objects.filter(id__in=transactions.values('organization_id')).distinct()
         return organizations
 
     @classmethod
     def get_user_totals(cls, client: User, start_day, end_day) -> dict:
         end_day = end_day + timedelta(days=1)
-        transactions = Transaction.objects.filter(updated_at__range=[start_day, end_day]).filter(
+        transactions = Transaction.objects.filter(created_at__range=[start_day, end_day]).filter(
             client=client).aggregate(total_original_amount=Coalesce(Sum('original_amount'), 0),
                                      total_savings=Coalesce(Sum('savings'), 0))
         return transactions

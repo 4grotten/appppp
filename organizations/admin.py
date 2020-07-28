cbd48242a2d7354d638dd6e716350da18566a671
dadaday <aaurub@gmail.com>
2020-07-28 19:25:01 +0600

    add phone, social links and cards inlines to OrganizationAdmin

--- organizations/admin.py
+++ organizations/admin.py
@@ -9,12 +9,29 @@ from .models import (
 )
 
 
+class PhoneInline(admin.TabularInline):
+    model = PhoneNumber
+    extra = 0
+
+
+class SocialInline(admin.TabularInline):
+    model = SocialNetworkContact
+    extra = 0
+
+
+class DiscountInline(admin.TabularInline):
+    model = DiscountCard
+    extra = 0
+
+
 class OrganizationAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.PointField: {"widget": GooglePointFieldWidget}
     }
     list_display = ('title', 'owner', 'opens_at', 'closes_at', 'currency', 'address', 'is_active')
     list_filter = ('is_active',)
+
+    inlines = (PhoneInline, SocialInline, DiscountInline,)
 
 
 class OrganizationTypeAdmin(admin.ModelAdmin):

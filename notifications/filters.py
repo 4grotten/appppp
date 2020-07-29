66d42f5f4fa59bfc0c75b03784ce680331c1d24f
Yrysbek Sagyndykov <Sagyndykov1998@gmail.com>
2020-07-29 19:42:35 +0600

    Adds 'mode' filter to notifications

---
+++ notifications/filters.py
@@ -0,0 +1,9 @@
+from django_filters import rest_framework as filters
+
+from notifications.models import Notification
+
+
+class NotificationFilter(filters.FilterSet):
+    class Meta:
+        model = Notification
+        fields = ('mode',)
--- notifications/views.py
+++ notifications/views.py
@@ -1,6 +1,8 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 
+from .filters import NotificationFilter
 from .serializers import NotificationSerializer
 from .services import NotificationService
 
@@ -8,6 +10,8 @@ from .services import NotificationService
 class NotificationListAPIView(ListAPIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = NotificationSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_class = NotificationFilter
 
     def get_queryset(self):
         return NotificationService.get_own_notifications(user=self.request.user)

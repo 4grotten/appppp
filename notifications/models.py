6e5342536281549ca2bf18d9bd5dedd9379f9795
Yrysbek Sagyndykov <Sagyndykov1998@gmail.com>
2020-07-30 19:24:16 +0600

    set primary_key to notification mode

--- notifications/models.py
+++ notifications/models.py
@@ -15,8 +15,7 @@ User = get_user_model()
 
 
 class NotificationMode(TimestampModel):
-    name = models.CharField(max_length=255)
-    type = models.CharField(max_length=20, choices=NOTIFICATION_MODES)
+    name = models.CharField(max_length=255, primary_key=True)
 
     def __str__(self):
         return self.name

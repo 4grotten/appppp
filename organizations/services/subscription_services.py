49637812b0ea2a6c909f7bd3e6303027501a2d61
Aibek Raiymbekov <aibek.raiymbekov01@gmail.com>
2020-08-01 23:16:32 +0600

    some changes

--- organizations/services/subscription_services.py
+++ organizations/services/subscription_services.py
@@ -21,24 +21,24 @@ class SubscriptionService:
     def toggle_subscription_status(cls, organization: Organization, user: User) -> bool:
         subscription, created = Subscription.objects.get_or_create(organization=organization, user=user)
         if created:
-            # NotificationService.create_notification(
-            #     recipient=organization.owner,
-            #     sender=user,
-            #     mode=SUBSCRIPTION_NOTIFICATION_MODE,
-            #     notification_type=FOLLOWED_TO_ORGANIZATION_TYPE,
-            #     title=FOLLOWED_TO_ORGANIZATION_TITLE,
-            #     description=organization.address,
-            #     organization=organization
-            # )
-            #
-            # NotificationService.create_notification(
-            #     recipient=user,
-            #     mode=SUBSCRIPTION_NOTIFICATION_MODE,
-            #     notification_type=ORGANIZATION_FOLLOWED_TYPE,
-            #     title=ORGANIZATION_FOLLOWED_TITLE,
-            #     description=organization.address,
-            #     organization=organization
-            # )
+            NotificationService.create_notification(
+                recipient=organization.owner,
+                sender=user,
+                mode=SUBSCRIPTION_NOTIFICATION_MODE,
+                notification_type=FOLLOWED_TO_ORGANIZATION_TYPE,
+                title=FOLLOWED_TO_ORGANIZATION_TITLE,
+                description=organization.address,
+                organization=organization
+            )
+
+            NotificationService.create_notification(
+                recipient=user,
+                mode=SUBSCRIPTION_NOTIFICATION_MODE,
+                notification_type=ORGANIZATION_FOLLOWED_TYPE.format(org_title=organization.title),
+                title=ORGANIZATION_FOLLOWED_TITLE,
+                description=organization.address,
+                organization=organization
+            )
 
             return True
         subscription.delete()

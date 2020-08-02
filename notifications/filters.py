4c21bfb70e89b7c32896fd7c465389498435b955
Aibek Raiymbekov <aibek.raiymbekov01@gmail.com>
2020-08-02 20:29:47 +0600

    notification filter

--- notifications/filters.py
+++ notifications/filters.py
@@ -1,9 +1,28 @@
 from django_filters import rest_framework as filters
 
+from common.exceptions import ValidationException
 from notifications.models import Notification
 
 
+class MultipleListFilter(filters.Filter):
+    MAX_LIMIT = 20
+
+    def filter(self, qs, value):
+        if not value:
+            return qs
+
+        self.lookup_expr = 'in'
+        values = value.split(',')
+
+        if len(values) > self.MAX_LIMIT:
+            raise ValidationException('Max number of ids should be less than equal 20')
+
+        return super(MultipleListFilter, self).filter(qs, values).distinct()
+
+
 class NotificationFilter(filters.FilterSet):
+    mode = MultipleListFilter(field_name='mode__name')
+
     class Meta:
         model = Notification
-        fields = ()
+        fields = ('mode',)

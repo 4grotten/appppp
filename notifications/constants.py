5bb543c3614cf878b9784d37fd0d4976d188ae99
Yrysbek Sagyndykov <Sagyndykov1998@gmail.com>
2020-08-03 23:17:43 +0600

    Added notification when request for partnership

--- notifications/constants.py
+++ notifications/constants.py
@@ -10,6 +10,10 @@ NOTIFICATION_MODES = (
     (PARTNER_MODE, PARTNER_MODE.capitalize())
 )
 
+ACCEPT_PARTNER_TYPE = 'accept_partner'
+DECLINE_PARTNER_TYPE = 'decline_partner'
+REQUEST_PARTNER_TYPE = 'request_partner'
+
 ACCEPT_DISCOUNT_TYPE = 'accept_discount'
 DECLINE_DISCOUNT_TYPE = 'decline_discount'
 NEW_DISCOUNT_TYPE = 'new_discount'
@@ -33,5 +37,7 @@ FOLLOWED_TO_ORGANIZATION_TITLE = 'На вашу организацию подп�
 ORGANIZATION_FOLLOWED_TITLE = 'Вы подписались на {org_title}'
 DISCOUNT_COMPLETE_USER_TITLE = 'Вам провели скидку {discount_percent} %'
 DISCOUNT_COMPLETE_DESCRIPTION = 'Итого со скидкой: {final_amount} {currency}'
-
 DISCOUNT_COMPLETE_TITLE = 'Вы провели скидку {discount_percent} %'
+
+PARTNER_REQUEST_TITLE = '{sender_organization} хочет стать партнером {recipient_organization}'
+PARTNER_REQUEST_DESCRIPTION = '{address}'
--- organizations/services/partnership_services.py
+++ organizations/services/partnership_services.py
@@ -4,6 +4,9 @@ from django.db import IntegrityError
 from django.db.models import QuerySet
 
 from common.exceptions import NotAcceptableException, IntegrityException, ObjectNotFoundException
+from notifications.constants import PARTNER_MODE, REQUEST_PARTNER_TYPE, PARTNER_REQUEST_TITLE, \
+    PARTNER_REQUEST_DESCRIPTION
+from notifications.services import NotificationService
 from organizations.models import Organization, Partnership
 from organizations.services.organization_services import OrganizationService
 from users.models import User
@@ -29,7 +32,16 @@ class PartnershipService:
         if not OrganizationService.user_can_edit_organization(organization=requested_by, user=user):
             raise NotAcceptableException('No rights to edit organization')
         cls.create(requested_by=requested_by, accepted_by=accepted_by)
-        # ToDo: send notification to accepted_by organization
+        NotificationService.create_notification(
+            recipient=accepted_by.owner,
+            sender=requested_by.owner,
+            mode=PARTNER_MODE,
+            notification_type=REQUEST_PARTNER_TYPE,
+            title=PARTNER_REQUEST_TITLE.format(sender_organization=requested_by.title,
+                                               recipient_organization=accepted_by.title),
+            description=PARTNER_REQUEST_DESCRIPTION.format(address=requested_by.address),
+            organization=requested_by
+        )
 
     @classmethod
     def are_partners(cls, requested_by: Organization, accepted_by: Organization) -> bool:

e4e72b157957e9b47fe5e210a34fecfcc82cd3f7
Aibek Raiymbekov <aibek.raiymbekov01@gmail.com>
2020-08-05 19:26:31 +0600

    changed mode

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -1,7 +1,10 @@
 from celery import shared_task
 from django.contrib.auth import get_user_model
 
-from notifications.constants import SUBSCRIPTION_NOTIFICATION_MODE, NEW_ORGANIZATION, NEW_ORGANIZATION_TITLE
+from notifications.constants import (
+    SUBSCRIPTION_NOTIFICATION_MODE, NEW_ORGANIZATION,
+    NEW_ORGANIZATION_TITLE, SYSTEM_NOTIFICATION_MODE
+)
 from notifications.services import NotificationService
 from organizations.models import Organization
 
@@ -15,7 +18,7 @@ def send_notifications_to_all_users(organization: Organization, user: User):
     for user in users:
         NotificationService.create_notification(
             recipient=user,
-            mode=SUBSCRIPTION_NOTIFICATION_MODE,
+            mode=SYSTEM_NOTIFICATION_MODE,
             notification_type=NEW_ORGANIZATION,
             title=NEW_ORGANIZATION_TITLE,
             description=organization.address,

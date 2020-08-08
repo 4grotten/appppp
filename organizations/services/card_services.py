0148497d6550e6740328bd8445d27b9b5cb2b021
Yrysbek Sagyndykov <Sagyndykov1998@gmail.com>
2020-08-08 10:50:23 +0600

    remove coditions to get recipient

--- organizations/services/card_services.py
+++ organizations/services/card_services.py
@@ -130,8 +130,6 @@ class DiscountCardService:
         if should_organize:
             cls.organize_cumulative_cards(organization=organization)
 
-
-
     @classmethod
     @transaction.atomic
     def organize_cumulative_cards(cls, organization: Organization):

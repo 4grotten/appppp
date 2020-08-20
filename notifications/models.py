1fed4b9164c64049b0a16eea0aa04d5edc69069c
Aibek Raiymbekov <aibek.raiymbekov01@gmail.com>
2020-08-20 19:23:55 +0600

    small to medium image

--- notifications/models.py
+++ notifications/models.py
@@ -97,7 +97,7 @@ class Notification(TimestampModel):
 
     @staticmethod
     def get_organization_small_image(organization):
-        return HOST_URL + str(organization.image.small) if organization.image else None
+        return HOST_URL + str(organization.image.medium) if organization.image else None
 
 
 class NotificationSetting(TimestampModel):

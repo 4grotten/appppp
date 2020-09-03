b69d019ef0b9028183a8e925160c5c29f73f2f84
Aibek Raiymbekov <aibek.raiymbekov01@gmail.com>
2020-09-03 15:33:22 +0600

    transaction filter fixed

--- transactions/services/transaction_services.py
+++ transactions/services/transaction_services.py
@@ -153,7 +153,8 @@ class TransactionService:
     def get_organization_transactions(cls, organization: Organization, processed_by: User = None,
                                       start_date=None, end_date=None, search_id: int = None):
 
-        transactions = Transaction.objects.filter()
+        transactions = Transaction.objects.filter(organization=organization, is_processed=True)
+
         if processed_by is not None:
             transactions = transactions.filter(processed_by=processed_by)
 

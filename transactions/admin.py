7a528e2b45289c24b760715f710b868521d92344
dadaday <aaurub@gmail.com>
2020-09-17 02:20:17 +0600

    minor changes in `TransactionAdmin`

--- transactions/admin.py
+++ transactions/admin.py
@@ -6,8 +6,8 @@ from .models import Transaction
 class TransactionAdmin(admin.ModelAdmin):
     list_display = (
         'id', 'client', 'organization', 'processed_by', 'is_processed', 'currency',
-        'original_amount', 'discount_percent', 'savings', 'from_cashback', 'final_amount', 'discount_type',
-        'created_at', 'updated_at',
+        'original_amount', 'discount_percent', 'savings', 'from_cashback', 'to_cashback', 'final_amount',
+        'discount_type', 'created_at', 'updated_at',
     )
     list_filter = (
         'is_processed', 'discount_percent', 'discount_type',
@@ -15,9 +15,13 @@ class TransactionAdmin(admin.ModelAdmin):
     )
     search_fields = ('id',)
     readonly_fields = (
-        'is_processed', 'discount_percent', 'savings', 'final_amount',
+        'is_processed', 'discount_percent', 'savings',
+        'from_cashback', 'to_cashback', 'final_amount',
         'source_card', 'created_at', 'updated_at',
     )
+
+    def has_change_permission(self, request, obj=None):
+        return False
 
 
 admin.site.register(Transaction, TransactionAdmin)

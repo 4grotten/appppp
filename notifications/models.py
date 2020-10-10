0071fbc94dff52f1a3d356b9d85dc7c21675fbd9
Aibek Raiymbekov <aibek.raiymbekov01@gmail.com>
2020-10-10 18:06:02 +0600

    changed dto

--- notifications/models.py
+++ notifications/models.py
@@ -92,8 +92,7 @@ class Notification(TimestampModel):
                 'extra_data': extra_data,
                 'type': type
             },
-            'icon': cls.get_organization_small_image(organization=organization) if organization else None,
-            'image': cls.get_organization_small_image(organization=organization) if organization else None
+            'icon': cls.get_organization_small_image(organization=organization) if organization else None
         }
         notification_payload_ru = {
             'title': title_ru,
@@ -109,8 +108,7 @@ class Notification(TimestampModel):
                 'extra_data': extra_data,
                 'type': type
             },
-            'icon': cls.get_organization_small_image(organization=organization) if organization else None,
-            'image': cls.get_organization_small_image(organization=organization) if organization else None
+            'icon': cls.get_organization_small_image(organization=organization) if organization else None
         }
 
         fcm_devices_ru = notification_setting.fcm_device.filter(settingstotoken__language='ru')

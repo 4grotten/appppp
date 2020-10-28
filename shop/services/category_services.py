5cab27c7f3f3e220bc48c48bce417d7fb58b3fe6
dadaday <aaurub@gmail.com>
2020-10-29 05:34:34 +0600

    do not subcategories if all items in it are hidden

--- shop/services/category_services.py
+++ shop/services/category_services.py
@@ -9,7 +9,7 @@ class ItemSubcategoryService:
         subcategories = ItemSubcategory.objects.filter(
             Q(organization__isnull=True) | Q(organization_id=organization_id))
 
-        item_filters = Q(items_in_category__organization__isnull=True) | Q(
-            items_in_category__organization_id=organization_id)
+        item_filters = Q(items_in_category__is_published=True) & (
+                Q(items_in_category__organization__isnull=True) | Q(items_in_category__organization_id=organization_id))
 
         return subcategories.annotate(items_count=Count('items_in_category', item_filters)).filter(items_count__gt=0)

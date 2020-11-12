3908b3aa3f5c88a5ef01121527d3051bdc79a495
dadaday <aaurub@gmail.com>
2020-11-12 22:13:53 +0600

    added `FeedItemOrderingFilter` to exclude shop items without price if ordering by price

--- shop/views/feed_views.py
+++ shop/views/feed_views.py
@@ -1,11 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from common.exceptions import NotAcceptableException
 from organizations.serializers.query_param_serializers import OrganizationQueryParamSerializer
-from shop.filters import FeedItemFilter
+from shop.filters import FeedItemFilter, FeedItemOrderingFilter
 from shop.models import ShopItem
 from shop.serializers.item_serializers import ItemFeedSerializer, ItemListSerializer
 from shop.services.item_services import ShopItemService
@@ -13,7 +13,7 @@ from shop.services.item_services import ShopItemService
 
 class FeedView(ListAPIView):
     serializer_class = ItemFeedSerializer
-    filter_backends = (DjangoFilterBackend, OrderingFilter, SearchFilter,)
+    filter_backends = (DjangoFilterBackend, FeedItemOrderingFilter, SearchFilter,)
     filterset_fields = ('subcategory', 'subcategory__category', 'organization__country', 'organization__city',)
     ordering_fields = ['updated_at', 'price']
     ordering = ['-updated_at']

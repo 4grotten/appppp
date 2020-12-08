07f19227bd1a3289b19a89d220973942afc0f4a0
Yrysbek Sagyndykov <Sagyndykov1998@gmail.com>
2020-12-08 13:02:51 +0600

    changed post and put response

--- organizations/services/organization_services.py
+++ organizations/services/organization_services.py
@@ -449,7 +449,6 @@ class OrganizationInstagramIntegrationService:
         try:
             username = get_username_from_instagram_url(url)
             user_info = get_instagram_user_info(username)
-            user_info['url'] = url
             InstagramIntegration.objects.create(organization=organization, url=url)
             return user_info
         except:
@@ -469,7 +468,6 @@ class OrganizationInstagramIntegrationService:
         try:
             username = get_username_from_instagram_url(url)
             user_info = get_instagram_user_info(username)
-            user_info['url'] = url
             insta = InstagramIntegration.objects.get(organization=organization)
             insta.url = url
             insta.save()

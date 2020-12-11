4db79ca2440fb315dc4701c373a13e072f5673b3
Yrysbek Sagyndykov <Sagyndykov1998@gmail.com>
2020-12-11 16:41:58 +0600

    fix

--- project/settings/base.py
+++ project/settings/base.py
@@ -176,7 +176,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 IMAGEKIT_DEFAULT_CACHEFILE_BACKEND = 'imagekit.cachefiles.backends.Async'
 IMAGEKIT_DEFAULT_CACHEFILE_STRATEGY = 'imagekit.cachefiles.strategies.Optimistic'
 
-CELERY_ACCEPT_CONTENT = ['json', 'application/text', 'pickle']
+CELERY_ACCEPT_CONTENT = ['json', 'application/text']
 
 AUTH_USER_MODEL = 'users.User'
 

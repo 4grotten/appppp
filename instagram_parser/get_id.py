0f634f37a0d4a65d7930cd9282dde7855239d2c1
Yrysbek Sagyndykov <Sagyndykov1998@gmail.com>
2020-12-11 15:49:15 +0600

    added pickle

--- instagram_parser/get_id.py
+++ instagram_parser/get_id.py
@@ -17,4 +17,4 @@ def usernametoid(name):
 
 def get_username_from_instagram_url(url: str) -> str:
     return (re.findall('(?:https?:)?\/\/(?:www\.)?(?:instagram\.com|instagr\.am)\/(?P<username>[A-Za-z0-9_](?:(?:['
-                       'A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_])))', url))[0]
+'A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_])))', url))[0]

534c6b766918bc26946f48bea4bfec635695a90b
Yrysbek Sagyndykov <Sagyndykov1998@gmail.com>
2020-12-11 16:33:29 +0600

    added proxy

--- instagram_parser/get_id.py
+++ instagram_parser/get_id.py
@@ -4,9 +4,11 @@ import re
 
 
 def usernametoid(name):
+    proxies = {'http': 'socks5://Selbulaone0912:B8g4KqW@89.191.233.151:45786', }
     try:
-        response = requests.get('https://www.instagram.com/' + name +"/?__a=1", headers={
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Geko/20100101 Firefox/64.0'}).json()
+        response = requests.get('https://www.instagram.com/' + name + "/?__a=1", headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Geko/20100101 Firefox/64.0'},
+                                proxies=proxies).json()
         if response:
             return response['graphql']['user']['id']
     except IndexError:
@@ -17,4 +19,4 @@ def usernametoid(name):
 
 def get_username_from_instagram_url(url: str) -> str:
     return (re.findall('(?:https?:)?\/\/(?:www\.)?(?:instagram\.com|instagr\.am)\/(?P<username>[A-Za-z0-9_](?:(?:['
-'A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_])))', url))[0]
+                       'A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_])))', url))[0]

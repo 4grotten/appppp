1f17b0a79262e26a7cbb412cee7d6180200c30a1
Yrysbek Sagyndykov <Sagyndykov1998@gmail.com>
2020-12-16 17:48:03 +0600

    fix

--- organizations/tasks.py
+++ organizations/tasks.py
@@ -1,7 +1,7 @@
 from datetime import date, timedelta, datetime
 from django.utils import timezone
 from instagram_parser import parser
-# from instagram_parser.get_id import get_username_from_instagram_url, usernametoid
+from instagram_parser.get_id import get_username_from_instagram_url, usernametoid
 from shop.models import ShopItem
 from celery import shared_task
 from organizations.models import Organization
@@ -10,9 +10,9 @@ from organizations.models import Organization
 @shared_task
 def parse_instagram_to_shop_items(organization_id: int, url: str):
     organization = Organization.objects.get(id=organization_id)
-    # username = get_username_from_instagram_url(url)
-    # user_id = usernametoid(username)
-    instagram_posts = parser.get_posts(123213)
+    username = get_username_from_instagram_url(url)
+    user_id = usernametoid(username)
+    instagram_posts = parser.get_posts(user_id)
     for instagram in instagram_posts:
         if not ShopItem.objects.filter(
                 created_at=timezone.make_aware(datetime.utcfromtimestamp(instagram.get('created_at'))),

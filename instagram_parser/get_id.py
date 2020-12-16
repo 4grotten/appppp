76b6f1f0a46a01a70b334b81a856a48fe57e22a2
Yrysbek Sagyndykov <Sagyndykov1998@gmail.com>
2020-12-16 16:27:32 +0600

    fix

--- instagram_parser/get_id.py
+++ instagram_parser/get_id.py
@@ -1,21 +1,21 @@
-import re
-
-from instagrapi import Client
-
-cl = Client()
-cl.login('ss115test', 'passisnotsecret')
-settings_for_login = cl.get_settings()
-
-
-def usernametoid(name: str):
-    try:
-        return cl.user_id_from_username(name)
-    except IndexError:
-        return "Wrong username!"
-    except ConnectionError:
-        return "Connection error!"
-
-
-def get_username_from_instagram_url(url: str) -> str:
-    return (re.findall('(?:https?:)?\/\/(?:www\.)?(?:instagram\.com|instagr\.am)\/(?P<username>[A-Za-z0-9_](?:(?:['
-                       'A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_])))', url))[0]
+# import re
+#
+# from instagrapi import Client
+#
+# cl = Client()
+# cl.login('ss115test', 'passisnotsecret')
+# settings_for_login = cl.get_settings()
+#
+#
+# def usernametoid(name: str):
+#     try:
+#         return cl.user_id_from_username(name)
+#     except IndexError:
+#         return "Wrong username!"
+#     except ConnectionError:
+#         return "Connection error!"
+#
+#
+# def get_username_from_instagram_url(url: str) -> str:
+#     return (re.findall('(?:https?:)?\/\/(?:www\.)?(?:instagram\.com|instagr\.am)\/(?P<username>[A-Za-z0-9_](?:(?:['
+#                        'A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_])))', url))[0]

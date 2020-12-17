ee305f0b0239b909649d5468b7c171401a3aa921
Yrysbek Sagyndykov <Sagyndykov1998@gmail.com>
2020-12-17 14:17:54 +0600

    Added new endpoint to save images from urls

--- common/urls.py
+++ common/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from common.views import ImageCreateView, CountriesListView, CountryCitySearchView, WatermarkImageCreateView
+from common.views import ImageCreateView, CountriesListView, CountryCitySearchView, WatermarkImageCreateView, \
+    ImageCreateFromUrlView
 
 urlpatterns = [
     path('files/', ImageCreateView.as_view(), name='images'),
+    path('save_image_from_url/', ImageCreateFromUrlView.as_view(), name='image_from_url'),
     path('watermarked_images/', WatermarkImageCreateView.as_view(), name='watermarked_images'),
     path('countries/', CountriesListView.as_view(), name='countries'),
     path('countries_and_cities/', CountryCitySearchView.as_view(), name='countries_cities_search'),

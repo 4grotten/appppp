7d20bbba92872bc59e483c4956e92bcae310238e
Yrysbek Sagyndykov <Sagyndykov1998@gmail.com>
2020-12-23 15:54:00 +0600

    changed instagram user

--- instagram_parser/user_info.py
+++ instagram_parser/user_info.py
@@ -3,21 +3,20 @@ from instagrapi import Client
 
 
 def get_instagram_user_info(username: str):
-    settings = {"uuids": {"phone_id": "35729fff-aa1e-4d6a-b889-1710df661a34",
-                          "uuid": "887b0e44-3b83-4996-af14-b517c96fee2f",
-                          "client_session_id": "1b9cd485-5107-4e20-9bae-28a695998d1c",
-                          "advertising_id": "3723cd4c-d370-4073-8507-1eb7dbb32699",
-                          "device_id": "android-71b445d43959effc"},
-                "cookies": {"csrftoken": "cM4BSEdZA3FQ6bXIKcZEZ8CPiRJxRPWP", "ds_user": "ss115test",
-                            "ds_user_id": "44745007017", "mid": "X9napgABAAHAkPJXqGNLXYjl13_J", "rur": "PRN",
-                            "sessionid": "44745007017%3AMBJ3TclCka5jaR%3A1",
-                            "urlgen": "\"{158.181.250.169: 41750}:1kpTbP:boqWK79fxV5PWsmtzCmQdrw0xRA\""},
-                "last_login": 1608112813.279875,
-                "device_settings": {"app_version": "105.0.0.18.119", "android_version": 28,
-                                    "android_release": "9.0", "dpi": "640dpi", "resolution": "1440x2560",
-                                    "manufacturer": "samsung", "device": "SM-G965F", "model": "star2qltecs",
-                                    "cpu": "samsungexynos9810", "version_code": "168361634"},
-                "user_agent": "Instagram 105.0.0.18.119 Android (28/9.0; 640dpi; 1440x2560; samsung; SM-G965F; star2qltecs; samsungexynos9810; en_US; 168361634)"}
+    settings = {
+        'uuids': {'phone_id': '6605a3d5-a2a7-44f2-b7b1-ba05c979572b', 'uuid': 'c24cfd46-dac3-4135-8064-d7453ee2f5ef',
+                  'client_session_id': '27624802-1d50-4bfa-ab8e-ad046199e940',
+                  'advertising_id': '73967dfc-1605-4b65-9e6a-7bc710d2ee1f', 'device_id': 'android-9adddd55865dc2c7'},
+        'cookies': {'csrftoken': 'LwRgs1JTDTZWnOa0fetR0K3ALXXhmYHq', 'ds_user': 'apofi_z', 'ds_user_id': '45149694492',
+                    'mid': 'X-MTIQABAAEzDuxYB6ZRM1JnMjaa', 'rur': 'RVA',
+                    'sessionid': '45149694492%3AjIq4vOZE7WPR9m%3A6',
+                    'urlgen': '"{158.181.250.169: 41750}:1ks0nv:CVG1dBvOC1K83neIbFUoipx6mi4"'},
+        'last_login': 1608717097.4080002,
+        'device_settings': {'app_version': '105.0.0.18.119', 'android_version': 28, 'android_release': '9.0',
+                            'dpi': '640dpi', 'resolution': '1440x2560', 'manufacturer': 'samsung', 'device': 'SM-G965F',
+                            'model': 'star2qltecs', 'cpu': 'samsungexynos9810', 'version_code': '168361634'},
+        'user_agent': 'Instagram 105.0.0.18.119 Android (28/9.0; 640dpi; 1440x2560; samsung; SM-G965F; star2qltecs; samsungexynos9810; en_US; 168361634)'}
+
     cl = Client(settings=settings)
     user_id = usernametoid(username)
     user_info = dict(cl.user_info(user_id=user_id))

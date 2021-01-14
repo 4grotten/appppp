0acafc7639db51a094b5d160ed58c2395c9b6c05
Yrysbek Sagyndykov <Sagyndykov1998@gmail.com>
2021-01-14 15:38:41 +0600

    in transactions details queryset removed is_processed=True condition

--- transactions/services/transaction_services.py
+++ transactions/services/transaction_services.py
@@ -52,7 +52,7 @@ class TransactionService:
 
     @classmethod
     def get_transaction(cls, transaction_id: int, requested_by: User) -> Transaction:
-        instance = cls.get(id=transaction_id, is_processed=True)
+        instance = cls.get(id=transaction_id)
 
         if instance.client != requested_by and not OrganizationService.user_can_see_stats(
                 organization=instance.organization, user=requested_by):

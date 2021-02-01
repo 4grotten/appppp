0826cad1a1538b1bd0c1484708b597b8021aa149
Roman Kornev <me@romanius.me>
2021-02-01 16:24:58 +0600

    Changed to expectFailure

--- transactions/tests/test_complete_transaction.py
+++ transactions/tests/test_complete_transaction.py
@@ -2,7 +2,7 @@ import json
 
 from django.urls import reverse
 from rest_framework.test import APITestCase
-from unittest import skip
+from unittest import expectedFailure
 
 from common.tests.factories import CurrencyFactory
 from organizations.models import OrganizationClientFinancialStatus, DiscountCard
@@ -14,7 +14,7 @@ from transactions.tests.factories import TransactionFactory
 from users.tests.factories import UserFactory, TokenFactory
 
 
-@skip
+@expectedFailure
 class CompleteTransactionTestCase(APITestCase):
     def setUp(self):
         self.url = reverse('v1:transaction_complete')

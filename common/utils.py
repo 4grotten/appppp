7628aa112582fee53feb1820240647d7753c1193
Roman Kornev <me@romanius.me>
2021-02-15 22:04:25 +0600

    Added imagekit file path generation to {filename}-{hash} format Imagekit temporary bugfix using repository from pull request, details: https://github.com/matthewwithanm/django-imagekit/pull/517

--- common/utils.py
+++ common/utils.py
@@ -2,9 +2,13 @@ import posixpath
 import hashlib
 import datetime
 import uuid
+import os
 from pathlib import Path
+from django.conf import settings
 
 from django.utils.crypto import get_random_string
+from imagekit.cachefiles.namers import hash, source_name_dot_hash
+from imagekit.utils import suggest_extension
 
 LENGTH_OF_NUMBER = 6
 ALLOWED_SYMBOLS = '123456789'
@@ -39,6 +43,22 @@ def upload_file_with_unique_name(instance, filename):
     ) / Path(str(uuid.uuid4())).with_suffix(
         Path(filename).suffix
     )
+
+
+def imagekit_filename_generator(generator):
+    # 'media/45428685bacab859/a2d7a138-6861-4609-b56e-3dbf97f0db4f.jpg'
+    source_filename = getattr(generator.source, 'name', None)
+
+    if source_filename is None or os.path.isabs(source_filename):
+        return hash(generator)
+    else:
+        path = Path(source_filename)
+
+        return str(
+            path.with_name(f'{path.stem}-{generator.get_hash()[0:8]}').with_suffix(
+                suggest_extension(source_filename or '', generator.format)
+            )
+        )
 
 
 def method_permission_classes(classes):

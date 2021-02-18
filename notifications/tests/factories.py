dbbefaae79d6d0b420d83ba00b2eee123f057e0d
yrysbek <Sagynydkov1998@gmail.com>
2021-02-18 11:50:03 +0600

    - Tests

---
+++ notifications/tests/factories.py
@@ -0,0 +1,39 @@
+import factory
+import random
+
+from fcm_django.models import FCMDevice
+
+from notifications.constants import NOTIFICATION_TYPES, NOTIFICATION_MODES
+from notifications.models import NotificationSetting, Notification, NotificationMode
+from users.tests.factories import UserFactory
+
+DEVICE_TYPES = ['ios', 'android', 'web']
+
+
+class NotificationSettingFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = NotificationSetting
+
+
+class NotificationModeFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = NotificationMode
+
+
+class FCMDeviceFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = FCMDevice
+
+    registration_id = factory.Sequence(lambda n: f'registration_id{n}')
+    type = factory.Sequence(lambda n: DEVICE_TYPES[random.randint(0, 2)])
+
+
+class NotificationFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Notification
+
+    mode = NotificationMode.objects.all().exclude(name='subscription')[random.randint(0, 4)]
+    type = factory.Sequence(lambda n: NOTIFICATION_TYPES[n % 37][1])
+    title = factory.Sequence(lambda n: f'Notifications {n}')
+    recipient = factory.SubFactory(UserFactory)
+    description = factory.Sequence(lambda n: f'Notification Description {n}')

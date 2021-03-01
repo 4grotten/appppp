1aa8bf7ebe025033d040b78831783fcfee9a6643
Ahmed Asadov <zoxon470@gmail.com>
2021-03-01 13:37:13 +0600

    #63 Add tests for HomepageSearchView

--- organizations/tests/test_home_page_search_view.py
+++ organizations/tests/test_home_page_search_view.py
@@ -1,78 +1,64 @@
-import json
 import os
-from django.urls import reverse
-from rest_framework.test import APITestCase
-from rest_framework import status
 
-from common.tests.factories import FileFactory
-from organizations.tests.factories import OrganizationFactory, PartnershipFactory
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from organizations.tests.factories import OrganizationFactory, \
+    PartnershipFactory
 from users.tests.factories import UserFactory
 
 
 class HomepageSearchViewTestCase(APITestCase):
-    maxDiff = None
-
     def setUp(self) -> None:
         self.url = reverse("v1:homepage_search")
         self.user = UserFactory()
+        self.org_one = OrganizationFactory()
+        self.org_two = OrganizationFactory()
 
-    # def test_get_organizations_without_partner_id(self):
-    #     expected_data = {
-    #         "total_count": 3,
-    #         "total_pages": 1,
-    #         "list": [
-    #             {
-    #                 "id": self.org_one.id,
-    #                 "image": {
-    #                     "id": self.org_one.image.id,
-    #                     "file": f"http://testserver{self.org_one.image.file.url}",
-    #                     "name": os.path.basename(str(self.org_one.image.file)),
-    #                     "large": f"http://testserver{self.org_one.image.large.url}",
-    #                     "medium": f"http://testserver{self.org_one.image.medium.url}",
-    #                     "small": f"http://testserver{self.org_one.image.small.url}",
-    #                 },
-    #                 "title": self.org_one.title,
-    #                 "types": [],
-    #                 "discounts": [],
-    #             },
-    #             {
-    #                 "id": self.org_two.id,
-    #                 "image": {
-    #                     "id": self.org_two.image.id,
-    #                     "file": f"http://testserver{self.org_two.image.file.url}",
-    #                     "name": os.path.basename(str(self.org_two.image.file)),
-    #                     "large": f"http://testserver{self.org_two.image.large.url}",
-    #                     "medium": f"http://testserver{self.org_two.image.medium.url}",
-    #                     "small": f"http://testserver{self.org_two.image.small.url}",
-    #                 },
-    #                 "title": self.org_two.title,
-    #                 "types": [],
-    #                 "discounts": [],
-    #             },
-    #             {
-    #                 "id": self.org_three.id,
-    #                 "image": {
-    #                     "id": self.org_three.image.id,
-    #                     "file": f"http://testserver{self.org_three.image.url}",
-    #                     "name": os.path.basename(str(self.org_three.image.file)),
-    #                     "large": f"http://testserver{self.org_three.image.large.url}",
-    #                     "medium": f"http://testserver{self.org_three.image.medium.url}",
-    #                     "small": f"http://testserver{self.org_three.image.small.url}",
-    #                 },
-    #                 "title": self.org_three.title,
-    #                 "types": [],
-    #                 "discounts": [],
-    #             }
-    #         ]
-    #     }
-    #
-    #     response = self.client.get(
-    #         self.url,
-    #         content_type='application/json'
-    #     )
-    #
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertJSONEqual(response.content, expected_data)
+    def test_get_organizations_without_partner_id(self):
+        expected_data = {
+            "total_count": 2,
+            "total_pages": 1,
+            "list": [
+                {
+                    "id": self.org_one.id,
+                    "image": {
+                        "id": self.org_one.image.id,
+                        "file": f"http://testserver{self.org_one.image.file.url}",
+                        "name": os.path.basename(str(self.org_one.image.file)),
+                        "large": f"http://testserver{self.org_one.image.large.url}",
+                        "medium": f"http://testserver{self.org_one.image.medium.url}",
+                        "small": f"http://testserver{self.org_one.image.small.url}",
+                    },
+                    "title": self.org_one.title,
+                    "types": [],
+                    "discounts": [],
+                },
+                {
+                    "id": self.org_two.id,
+                    "image": {
+                        "id": self.org_two.image.id,
+                        "file": f"http://testserver{self.org_two.image.file.url}",
+                        "name": os.path.basename(str(self.org_two.image.file)),
+                        "large": f"http://testserver{self.org_two.image.large.url}",
+                        "medium": f"http://testserver{self.org_two.image.medium.url}",
+                        "small": f"http://testserver{self.org_two.image.small.url}",
+                    },
+                    "title": self.org_two.title,
+                    "types": [],
+                    "discounts": [],
+                },
+            ]
+        }
+
+        response = self.client.get(
+            self.url,
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertJSONEqual(response.content, expected_data)
 
     def test_get_organizations_with_parameter_partner(self):
         partner = PartnershipFactory(
@@ -89,16 +75,16 @@ class HomepageSearchViewTestCase(APITestCase):
             "total_pages": 1,
             "list": [
                 {
-                    "id": self.org_one.id,
+                    "id": partner.accepted_by.id,
                     "image": {
-                        "id": partner.requested_by.image.id,
-                        "file": f"http://testserver{self.org_one.image.file.url}",
-                        "name": os.path.basename(str(partner.requested_by.image.file)),
-                        "large": f"http://testserver{partner.requested_by.image.large.url}",
-                        "medium": f"http://testserver{partner.requested_by.image.medium.url}",
-                        "small": f"http://testserver{partner.requested_by.image.small.url}",
+                        "id": partner.accepted_by.image.id,
+                        "file": f"http://testserver{partner.accepted_by.image.file.url}",
+                        "name": os.path.basename(str(partner.accepted_by.image.file)),
+                        "large": f"http://testserver{partner.accepted_by.image.large.url}",
+                        "medium": f"http://testserver{partner.accepted_by.image.medium.url}",
+                        "small": f"http://testserver{partner.accepted_by.image.small.url}",
                     },
-                    "title": partner.requested_by.title,
+                    "title": partner.accepted_by.title,
                     "types": [],
                     "discounts": [],
                 },
@@ -111,6 +97,5 @@ class HomepageSearchViewTestCase(APITestCase):
             content_type='application/json'
         )
 
-        print(response.content)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertJSONEqual(response.content, expected_data)

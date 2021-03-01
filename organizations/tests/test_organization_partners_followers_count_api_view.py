b2560ca983d60a100a1263b9d5827b799518e48c
Ahmed Asadov <zoxon470@gmail.com>
2021-03-02 00:27:05 +0600

    #67 remove attribute maxDiff from OrganizationPartnersFollowersCountAPIViewTestCase

--- organizations/tests/test_organization_partners_followers_count_api_view.py
+++ organizations/tests/test_organization_partners_followers_count_api_view.py
@@ -11,8 +11,6 @@ from users.tests.factories import UserFactory
 
 
 class OrganizationPartnersFollowersCountAPIViewTestCase(APITestCase):
-    maxDiff = None
-
     def setUp(self) -> None:
         self.user_one = UserFactory()
         self.user_two = UserFactory(

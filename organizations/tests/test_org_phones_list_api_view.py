4cea4413942ac26513eb450e1a5eca61e8d2d5d2
Ahmed Asadov <zoxon470@gmail.com>
2021-03-01 14:20:10 +0600

    #61 WIP

--- organizations/tests/test_org_phones_list_api_view.py
+++ organizations/tests/test_org_phones_list_api_view.py
@@ -15,89 +15,84 @@ from common.tests.factories import CurrencyFactory
 class OrgPhonesListAPIViewTestCase(APITestCase):
     maxDiff = None
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.currency = CurrencyFactory(code="USD", name="USD")
-
     def setUp(self) -> None:
         self.user = UserFactory(phone_number="996550778133")
         self.organization = OrganizationFactory(
-            owner=self.user,
-            currency=self.currency
+            owner=self.user
         )
 
-    def test_user_unauthorized(self):
-        expected_data = {
-            "detail": "Authentication credentials were not provided."
-        }
-
-        response = self.client.post(
-            reverse("v1:organization_phones", kwargs={"pk": self.organization.id}),
-            content_type='application/json'
-        )
-
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-        self.assertJSONEqual(response.content, expected_data)
-
-    def test_required_fields(self):
-        self.client.force_authenticate(user=self.user)
-        expected_data = {
-            "message": "Invalid input",
-            "errors": {
-                "phone_numbers": ["This field is required."]
-            }
-        }
-        response = self.client.post(
-            reverse("v1:organization_phones", kwargs={"pk": self.organization.id}),
-            data=json.dumps({}),
-            content_type='application/json'
-        )
-
-        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
-        self.assertJSONEqual(response.content, expected_data)
-
-    def test_get_list_organizations(self):
-        self.client.force_authenticate(user=self.user)
-        organization_one = OrganizationFactory(owner=self.user)
-        organization_two = OrganizationFactory(owner=self.user)
-        organization_three = OrganizationFactory(owner=self.user)
-        expected_data = {
-            "list": [{
-                "id": organization_one.id,
-                "image": organization_one.image,
-                "role": OrganizationService.get_user_role_in_organization(
-                    organization=organization_one, user=self.user
-                ),
-                "title": organization_one.title
-            },
-            {
-                "id": organization_two.id,
-                "image": organization_two.image,
-                "role": OrganizationService.get_user_role_in_organization(
-                    organization=organization_two, user=self.user
-                ),
-                "title": organization_two.title
-            },
-            {
-                "id": organization_three.id,
-                "image": organization_three.image,
-                "role": OrganizationService.get_user_role_in_organization(
-                    organization=organization_three, user=self.user
-                ),
-                "title": organization_three.title
-            }
-            ],
-            "total_count": 3,
-            "total_pages": 1
-        }
-
-        response = self.client.get(
-            reverse("v1:organization_phones", kwargs={"pk": self.organization.id}),
-            content_type='application/json'
-        )
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertJSONEqual(response.content, expected_data)
+    # def test_user_unauthorized(self):
+    #     expected_data = {
+    #         "detail": "Authentication credentials were not provided."
+    #     }
+    #
+    #     response = self.client.post(
+    #         reverse("v1:organization_phones", kwargs={"pk": self.organization.id}),
+    #         content_type='application/json'
+    #     )
+    #
+    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    #     self.assertJSONEqual(response.content, expected_data)
+    #
+    # def test_required_fields(self):
+    #     self.client.force_authenticate(user=self.user)
+    #     expected_data = {
+    #         "message": "Invalid input",
+    #         "errors": {
+    #             "phone_numbers": ["This field is required."]
+    #         }
+    #     }
+    #     response = self.client.post(
+    #         reverse("v1:organization_phones", kwargs={"pk": self.organization.id}),
+    #         data=json.dumps({}),
+    #         content_type='application/json'
+    #     )
+    #
+    #     self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
+    #     self.assertJSONEqual(response.content, expected_data)
+    #
+    # def test_get_list_organizations(self):
+    #     self.client.force_authenticate(user=self.user)
+    #     organization_one = OrganizationFactory(owner=self.user)
+    #     organization_two = OrganizationFactory(owner=self.user)
+    #     organization_three = OrganizationFactory(owner=self.user)
+    #     expected_data = {
+    #         "list": [{
+    #             "id": organization_one.id,
+    #             "image": organization_one.image,
+    #             "role": OrganizationService.get_user_role_in_organization(
+    #                 organization=organization_one, user=self.user
+    #             ),
+    #             "title": organization_one.title
+    #         },
+    #         {
+    #             "id": organization_two.id,
+    #             "image": organization_two.image,
+    #             "role": OrganizationService.get_user_role_in_organization(
+    #                 organization=organization_two, user=self.user
+    #             ),
+    #             "title": organization_two.title
+    #         },
+    #         {
+    #             "id": organization_three.id,
+    #             "image": organization_three.image,
+    #             "role": OrganizationService.get_user_role_in_organization(
+    #                 organization=organization_three, user=self.user
+    #             ),
+    #             "title": organization_three.title
+    #         }
+    #         ],
+    #         "total_count": 3,
+    #         "total_pages": 1
+    #     }
+    #
+    #     response = self.client.get(
+    #         reverse("v1:organization_phones", kwargs={"pk": self.organization.id}),
+    #         content_type='application/json'
+    #     )
+    #
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    #     self.assertJSONEqual(response.content, expected_data)
 
     def test_get_org_phones_list(self):
         self.client.force_authenticate(user=self.user)

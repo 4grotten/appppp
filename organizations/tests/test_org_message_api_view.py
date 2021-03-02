34f1546a444cf289a831c08ff45cbad5cdaa0348
Ahmed Asadov <zoxon470@gmail.com>
2021-03-02 20:20:07 +0600

    #68 WIP tests for OrgMessageAPIView

--- organizations/tests/test_org_message_api_view.py
+++ organizations/tests/test_org_message_api_view.py
@@ -1,21 +1,17 @@
-import json
 import os
 
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from common.tests.factories import FileFactory
-from organizations.models import DiscountCard, Message
-from organizations.services.organization_services import OrganizationService
-from organizations.tests.factories import OrganizationFactory, MessageFactory, MembershipFactory
+from organizations.models import Message
+from organizations.tests.factories import (
+    OrganizationFactory, MessageFactory, MembershipFactory
+)
 from users.tests.factories import UserFactory
-from unittest import expectedFailure
 
 
 class OrgMessageAPIViewTestCase(APITestCase):
-    maxDiff = None
-
     def setUp(self) -> None:
         self.user = UserFactory(
             full_name="Johnny Sins",
@@ -23,25 +19,25 @@ class OrgMessageAPIViewTestCase(APITestCase):
         )
         self.organization = OrganizationFactory()
 
-    # def test_user_unauthorized(self):
-    #     expected_data = {
-    #         "detail": "Authentication credentials were not provided."
-    #     }
-    #
-    #     response = self.client.get(
-    #         reverse("v1:organization_messages", kwargs={
-    #             "pk": self.organization.id
-    #         }),
-    #         content_type='application/json'
-    #     )
-    #
-    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-    #     self.assertJSONEqual(response.content, expected_data)
+    def test_user_unauthorized(self):
+        expected_data = {
+            "detail": "Authentication credentials were not provided."
+        }
+
+        response = self.client.get(
+            reverse("v1:organization_messages", kwargs={
+                "pk": self.organization.id
+            }),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertJSONEqual(response.content, expected_data)
 
     def test_get_organization_messages(self):
         self.client.force_authenticate(user=self.user)
-        user_receiver_one = UserFactory()
-        user_receiver_two = UserFactory()
+        user_receiver_one = UserFactory(full_name="Какой-то чертила")
+        user_receiver_two = UserFactory(full_name="Какой-то чертила номер два")
         organization_two = OrganizationFactory()
         organization_three = OrganizationFactory()
         message_one = MessageFactory(
@@ -64,47 +60,43 @@ class OrgMessageAPIViewTestCase(APITestCase):
             "list": [
                 {
                     "id": message_one.id,
-                    "title": organization_two.title,
-                    "content": message_one.content,
-                    "message_to": message_one.message_to,
-                    "receiver_partner_count": 2,
-                    "receiver_partners": [
-                        {
-                            "id": organization_two.id,
-                            "image": {
-                                "id": organization_two.owner.avatar.id,
-                                "file": f"http://testserver{organization_two.owner.avatar.file.url}",
-                                "name": os.path.basename(organization_two.owner.avatar.file.name),
-                                "large": f"http://testserver{organization_two.owner.avatar.large.url}",
-                                "medium": f"http://testserver{organization_two.owner.avatar.medium.url}",
-                                "small": f"http://testserver{organization_two.owner.avatar.small.url}",
-                            }
-                        },
-                        {
-                            "id": organization_three.id,
-                            "image": {
-                                "id": organization_three.owner.avatar.id,
-                                "file": f"http://testserver{organization_three.owner.avatar.file.url}",
-                                "name": os.path.basename(
-                                    organization_three.owner.avatar.file.name),
-                                "large": f"http://testserver{organization_three.owner.avatar.large.url}",
-                                "medium": f"http://testserver{organization_three.owner.avatar.medium.url}",
-                                "small": f"http://testserver{organization_three.owner.avatar.small.url}",
-                            }
+                    "sender": {
+                        "id": self.user.id,
+                        "full_name": self.user.full_name,
+                        "username": self.user.username,
+                        "avatar": {
+                            "id": self.user.avatar.id,
+                            "file": f"http://testserver{self.user.avatar.file.url}",
+                            "name": os.path.basename(
+                                self.user.avatar.file.name),
+                            "large": f"http://testserver{self.user.avatar.large.url}",
+                            "medium": f"http://testserver{self.user.avatar.medium.url}",
+                            "small": f"http://testserver{self.user.avatar.small.url}",
                         }
-                    ],
+                    },
+                    "message_to": message_one.message_to,
+                    "content": message_one.content,
+                    "created_at": message_one.created_at.strftime(
+                        "%Y-%m-%dT%H:%M:%S.%fZ"
+                    ),
+                    "receivers_count": 2,
                     "receivers": [
                         {
+                            "id": user_receiver_one.id,
+                            "full_name": user_receiver_one.full_name,
                             "avatar": {
                                 "id": user_receiver_one.avatar.id,
                                 "file": f"http://testserver{user_receiver_one.avatar.file.url}",
-                                "name": os.path.basename(user_receiver_one.avatar.file.name),
+                                "name": os.path.basename(
+                                    user_receiver_one.avatar.file.name),
                                 "large": f"http://testserver{user_receiver_one.avatar.large.url}",
                                 "medium": f"http://testserver{user_receiver_one.avatar.medium.url}",
                                 "small": f"http://testserver{user_receiver_one.avatar.small.url}",
                             }
                         },
                         {
+                            "id": user_receiver_two.id,
+                            "full_name": user_receiver_two.full_name,
                             "avatar": {
                                 "id": user_receiver_two.avatar.id,
                                 "file": f"http://testserver{user_receiver_two.avatar.file.url}",
@@ -116,21 +108,35 @@ class OrgMessageAPIViewTestCase(APITestCase):
                             }
                         }
                     ],
-                    "sender": {
-                        "id": self.user.id,
-                        "full_name": self.user.full_name,
-                        "username": self.user.username,
-                        "avatar": {
-                                "id": self.user.avatar.id,
-                                "file": f"http://testserver{self.user.avatar.file.url}",
-                                "name": os.path.basename(self.user.avatar.file.name),
-                                "large": f"http://testserver{self.user.avatar.large.url}",
-                                "medium": f"http://testserver{self.user.avatar.medium.url}",
-                                "small": f"http://testserver{self.user.avatar.small.url}",
-                            }
-                    },
                     "sender_role": membership.role.title,
-                    "created_at": message_one.created_at,
+                    "receiver_partner_count": 2,
+                    "receiver_partners": [
+                        {
+                            "id": organization_two.id,
+                            "title": organization_two.title,
+                            "image": {
+                                "id": organization_two.image.id,
+                                "file": f"http://testserver{organization_two.image.file.url}",
+                                "name": os.path.basename(organization_two.image.file.name),
+                                "large": f"http://testserver{organization_two.image.large.url}",
+                                "medium": f"http://testserver{organization_two.image.medium.url}",
+                                "small": f"http://testserver{organization_two.image.small.url}",
+                            }
+                        },
+                        {
+                            "id": organization_three.id,
+                            "title": organization_three.title,
+                            "image": {
+                                "id": organization_three.image.id,
+                                "file": f"http://testserver{organization_three.image.file.url}",
+                                "name": os.path.basename(
+                                    organization_three.image.file.name),
+                                "large": f"http://testserver{organization_three.image.large.url}",
+                                "medium": f"http://testserver{organization_three.image.medium.url}",
+                                "small": f"http://testserver{organization_three.image.small.url}",
+                            }
+                        }
+                    ],
                 }
             ]
         }
@@ -141,9 +147,6 @@ class OrgMessageAPIViewTestCase(APITestCase):
             }),
             content_type='application/json'
         )
-
-
-        print("=", response.content)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertJSONEqual(response.content, expected_data)

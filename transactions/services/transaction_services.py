de7e861458342ff7960f1c1d0118ff25a8146cb6
Ahmed Asadov <zoxon470@gmail.com>
2021-03-09 14:20:09 +0600

    #69 optimize database saving in method complete_online_transaction

--- transactions/services/transaction_services.py
+++ transactions/services/transaction_services.py
@@ -281,7 +281,17 @@ class TransactionService:
             current_transaction.status = Transaction.ACCEPTED
             current_transaction.original_amount = original_price
             current_transaction.savings = original_price - discounted_price
-            current_transaction.save()
+            current_transaction.save(update_fields=[
+                "is_processed",
+                "fixed_cart",
+                "processed_by",
+                "employee_role",
+                "employee_name",
+                "employee_avatar",
+                "status",
+                "original_amount",
+                "savings",
+            ])
         except IntegrityError:
             raise IntegrityException('Could not complete transaction')
         client_status = OrganizationClientFinancialStatusService.get_or_create(

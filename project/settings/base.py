0d45cda4a77c98baf12cf556d8b355050c749a80
Ahmed Asadov <zoxon470@gmail.com>
2021-03-13 00:08:35 +0600

    #88 Add configuration for sentry service

--- project/settings/base.py
+++ project/settings/base.py
@@ -291,6 +291,25 @@ if not DEBUG and JSON_LOGGING:
         }
     }
 
+# Sentry
+if not DEBUG:
+    SENTRY_DSN = config("SENTRY_DSN", cast=str)
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[DjangoIntegration()],
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production,
+        traces_sample_rate=1.0,
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
+
 if MONITORING:
     INSTALLED_APPS += ['django_prometheus']
     MIDDLEWARE = \

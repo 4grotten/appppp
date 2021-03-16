45231b62cf0853f16d223b414c5bb04cfaf31acd
Ahmed Asadov <zoxon470@gmail.com>
2021-03-16 21:54:20 +0600

    #92 Add upload_item_subcategories task

--- shop/tasks.py
+++ shop/tasks.py
@@ -1,6 +1,7 @@
-from instagram_parsers.parsers.parser import get_video_url_from_post
-from shop.models import ItemInstagramData
 from celery import shared_task
+
+from instagram_parsers.parsers.parser import get_video_url_from_post
+from shop.models import ItemInstagramData, ItemSubcategory
 
 
 @shared_task
@@ -10,3 +11,16 @@ def update_instagram_videos():
         video_url = get_video_url_from_post(data.post_pk)
         data.video_url = video_url
         data.save()
+
+
+@shared_task
+def upload_item_subcategories(csv_data, category_id):
+    splited_data = csv_data.splitlines()
+    for line in splited_data:
+        (category_ru, category_en, category_tr) = line.split(";")
+        _, _ = ItemSubcategory.objects.update_or_create(
+            category_id=category_id,
+            name_ru=category_ru,
+            name_en=category_en,
+            name_tr=category_tr
+        )

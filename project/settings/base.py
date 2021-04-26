9dc69a479b87c928737070336e4e3c8f94808df6
dadaday <aaurub@gmail.com>
2021-04-26 20:09:14 +0600

    use `insta_low` queue for `delete_old_instagram_posts` task

--- project/settings/base.py
+++ project/settings/base.py
@@ -272,6 +272,7 @@ CELERY_TASK_ROUTES = {
     'organizations.tasks.parse_instagram_to_shop_items': {'queue': 'insta_high'},
     'organizations.tasks.parse_instagram_last_updates': {'queue': 'insta_high'},
     'organizations.tasks.delete_not_updated_posts_from_instagram': {'queue': 'insta_low'},
+    'shop.tasks.delete_old_instagram_posts': {'queue': 'insta_low'},
     'shop.tasks.update_instagram_videos': {'queue': 'insta_low'},
 }
 

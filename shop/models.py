163aa2b837424e1adce02b4f83ea4dc639ff737b
alex <nuvorish81@gmail.com>
2021-07-21 23:21:36 +0600

    Added new feelds into ShopItem model to store lang.

--- shop/models.py
+++ shop/models.py
@@ -7,6 +7,7 @@ from django.utils import timezone
 from common.models import TimestampModel, File
 from organizations.models import Organization
 from transactions.models import Transaction
+from translator.translator import GoogleTranslator
 from users.models import User
 
 
@@ -44,7 +45,9 @@ class ShopItem(models.Model):
     subcategory = models.ForeignKey(ItemSubcategory, on_delete=models.SET_NULL, related_name='items_in_category',
                                     null=True, blank=True)
     name = models.CharField(max_length=64)
+    name_lang = models.CharField(max_length=4, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
+    description_lang = models.CharField(max_length=4, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     discount = models.PositiveSmallIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
     discounted_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, editable=False)
@@ -61,6 +64,9 @@ class ShopItem(models.Model):
         return f'{self.name}'
 
     def save(self, *args, **kwargs):
+        self.name_lang = GoogleTranslator().get_lang(self.name)
+        if self.description:
+            self.description_lang = GoogleTranslator().get_lang(self.description)
         if self.id:
             self.updated_at = timezone.now()
             self.is_updated = True
--- shop/views/item_views.py
+++ shop/views/item_views.py
@@ -1,6 +1,5 @@
 from django.db import IntegrityError
 from django.utils.translation import gettext_lazy as _
-from googletrans import Translator
 from rest_framework import status, permissions
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, GenericAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -18,6 +17,7 @@ from shop.serializers.other_serializers import ComplaintSerializer
 from shop.services.cart_services import CartItemService
 from shop.services.item_services import ShopItemService
 from shop.services.like_bookmark_services import LikeService, BookmarkService
+from translator.translator import GoogleTranslator
 
 
 class ItemCreateView(CreateAPIView):
@@ -132,11 +132,8 @@ class TranslateItemTextView(GenericAPIView):
         data = request.data
         lang = request.META.get('HTTP_ACCEPT_LANGUAGE', None)
         try:
-            random_proxy = ProxyService.get_random_formed_proxy(True).replace("https://", '')
-            proxies = {'http': random_proxy}
-            translator = Translator(proxies=proxies)
-            translate_name = translator.translate(data['title'], dest=lang)
-            translate_description = translator.translate(data['description'], dest=lang)
+            translate_name = GoogleTranslator().translate(data['title'], lang)
+            translate_description = GoogleTranslator().translate(data['description'], lang)
             return Response(data={
                 'title': translate_name.text,
                 'description': translate_description.text,

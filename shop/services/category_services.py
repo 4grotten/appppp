ed8f3365bd57f8dcaf54f0fd05b543149d815320
alex <nuvorish81@gmail.com>
2021-07-26 18:02:21 +0600

    Added filter in get_general_nonempty_subcategories_in_category() to exclude subcategories where hase item but company is banned.

--- shop/services/category_services.py
+++ shop/services/category_services.py
@@ -47,8 +47,9 @@ class ItemSubcategoryService:
     @classmethod
     def get_general_nonempty_subcategories_in_category(cls, category: ItemCategory, country: Union[Country, None],
                                                        city: Union[City, None]) -> QuerySet:
-        subcategories = ItemSubcategory.objects.filter(organization__isnull=True, category=category)
-
+        subcategories = ItemSubcategory.objects.filter(organization__isnull=True, category=category)\
+            .exclude(Q(items_in_category__organization__is_banned=True)
+                     | Q(items_in_category__organization__is_deleted=True))
         item_filters = Q(items_in_category__is_published=True) & Q(items_in_category__price__isnull=False)
         if city is not None:
             item_filters = item_filters & Q(items_in_category__organization__city=city)

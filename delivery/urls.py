e6c1a959711e9e28c55b0468475f52755c55fc74
Azamat Tokhtaev <krik123@gmail.com>
2021-07-27 12:06:19 +0600

    fix typo

--- delivery/urls.py
+++ delivery/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from delivery import views
 delivery_urls = [
     path('delivery/deliveryItemsCount/', views.DeliveryItemsCountView.as_view(), name='delivery_items_count'),
-    path('delivery/odersForDelivery/', views.DeliveryInfoListView.as_view(), name='delivery_orders'),
+    path('delivery/ordersForDelivery/', views.DeliveryInfoListView.as_view(), name='delivery_orders'),
 
 ]
 urlpatterns = [

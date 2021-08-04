11186ad559c58f0fc865c18494017447886826b9
alex <nuvoeish81@mail.ru>
2021-08-04 12:33:49 +0600

    change field  in admin model ItemInstagramDataAdmin.

--- shop/admin.py
+++ shop/admin.py
@@ -38,7 +38,7 @@ class ShopItemAdmin(admin.ModelAdmin):
 
 class ItemInstagramDataAdmin(admin.ModelAdmin):
     list_display = ('item', 'thumbnail_url', 'video_url', 'updated_at', 'created_at',)
-
+    raw_id_fields = ('item',)
 
 class ItemLikeAdmin(admin.ModelAdmin):
     list_display = ('user', 'item',)

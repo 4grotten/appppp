6a393b79539c30734f22f188c4fa73ec175ca17b
Azamat Tokhtaev <krik123@gmail.com>
2021-08-04 19:03:15 +0600

    Add api for status=delivered

--- delivery/urls.py
+++ delivery/urls.py
@@ -3,9 +3,9 @@ from delivery import views
 delivery_urls = [
     path('delivery/deliveryItemsCount/', views.DeliveryItemsCountView.as_view(), name='delivery_items_count'),
     path('delivery/ordersForDelivery/', views.DeliveryInfoListView.as_view(), name='delivery_orders'),
-    path('delivery/<int:pk>/acceptForDeliveryByCourier/', views.AcceptOrderForDeliveryByDeliveryService.as_view(), name='accept_for_delivery'),
-    path('delivery/<int:pk>/rejectDeliveryByCourier/', views.RejectOrderForDeliveryByDeliveryService.as_view(), name='accept_for_delivery'),
-
+    path('delivery/<int:pk>/acceptForDeliveryByCourier/', views.AcceptOrderForDeliveryByDeliveryServiceView.as_view(), name='accept_for_delivery'),
+    path('delivery/<int:pk>/rejectDeliveryByCourier/', views.RejectOrderForDeliveryByDeliveryServiceView.as_view(), name='reject_delivery'),
+    path('delivery/<int:pk>/delivered/', views.DeliveredByDeliveryServiceView.as_view(), name='delivered'),
 ]
 urlpatterns = [
     path('', include(delivery_urls)),
--- delivery/views.py
+++ delivery/views.py
@@ -29,18 +29,20 @@ class DeliveryInfoListView(ListAPIView):
         return DeliveryInfoService.get_available_orders(user)
 
 
-class AcceptOrderForDeliveryByDeliveryService(APIView):
+class AcceptOrderForDeliveryByDeliveryServiceView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
-        delivery_organization = request.user.owned_organizations.filter(is_delivery_service=True, is_active=True, is_banned=False, is_deleted=False).first()
+        delivery_organization = request.user.owned_organizations.filter(is_delivery_service=True, is_active=True,
+                                                                        is_banned=False, is_deleted=False).first()
         if not delivery_organization:
             return Response(data={
                 'message': _('This user is not delivery service'),
                 'errors': _("Not delivery service")
             }, status=status.HTTP_406_NOT_ACCEPTABLE)
         delivery_info = DeliveryInfo.objects.get(id=kwargs['pk'])
-        if delivery_info.status not in (DeliveryInfo.DELIVERY_STATUS_REJECTED_BY_DELIVERY_SERVICE, DeliveryInfo.DELIVERY_STATUS_SET_FOR_DELIVERY):
+        if delivery_info.status not in (
+        DeliveryInfo.DELIVERY_STATUS_REJECTED_BY_DELIVERY_SERVICE, DeliveryInfo.DELIVERY_STATUS_SET_FOR_DELIVERY):
             return Response(data={
                 'message': _('Already taken'),
                 'errors': _("Delivery is already taken")
@@ -52,8 +54,7 @@ class AcceptOrderForDeliveryByDeliveryService(APIView):
         return Response({'status': 'ok'})
 
 
-
-class RejectOrderForDeliveryByDeliveryService(APIView):
+class RejectOrderForDeliveryByDeliveryServiceView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
@@ -68,5 +69,35 @@ class RejectOrderForDeliveryByDeliveryService(APIView):
         delivery_info.status = DeliveryInfo.DELIVERY_STATUS_REJECTED_BY_DELIVERY_SERVICE
 
         delivery_info.save()
-        data = DeliveryInfoSerializer(delivery_info, context={'request': request})
+
+        return Response({'status': 'ok'})
+
+
+class DeliveredByDeliveryServiceView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, *args, **kwargs):
+        delivery_organization = request.user.owned_organizations.filter(is_delivery_service=True, is_active=True,
+                                                                        is_banned=False, is_deleted=False).first()
+        if not delivery_organization:
+            return Response(data={
+                'message': _('This user is not delivery service'),
+                'errors': _("Not delivery service")
+            }, status=status.HTTP_406_NOT_ACCEPTABLE)
+        delivery_info = DeliveryInfo.objects.get(id=kwargs['pk'])
+        if delivery_info.delivery_organization != delivery_organization:
+            return Response(data={
+                'message': _('This delivery service is not owner of this delivery'),
+                'errors': _("Not your order")
+            }, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+        if delivery_info.status == DeliveryInfo.DELIVERY_STATUS_DELIVERED:
+            return Response(data={
+                'message': _('This order is already delivered'),
+                'errors': _("Delivered")
+            }, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+        delivery_info.status = DeliveryInfo.DELIVERY_STATUS_DELIVERED
+        delivery_info.save()
+
         return Response({'status': 'ok'})

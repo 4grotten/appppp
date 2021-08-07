04b4a565640ddd8c4747b6b2a32f7908303c3b79
Azamat Tokhtaev <krik123@gmail.com>
2021-08-07 17:53:24 +0600

    Show if proxy is expired in a separate column

--- instagram_parsers/admin.py
+++ instagram_parsers/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
+from django.utils import timezone
 
 from instagram_parsers.models import Proxy, LoginDevice
 
 
 @admin.register(Proxy)
 class ProxyAdmin(admin.ModelAdmin):
-    list_display = ('id', 'http_s', 'socks5', 'login', 'password', 'for_getting_username', 'expires_at',)
+    list_display = ('id', 'http_s', 'socks5', 'login', 'password', 'for_getting_username', 'expires_at', 'is_active')
+
+    def is_active(self, obj):
+        now = timezone.now()
+        if obj.expires_at:
+            if now > obj.expires_at:
+                return False
+            else:
+                return True
+    is_active.boolean = True
 
 
 @admin.register(LoginDevice)

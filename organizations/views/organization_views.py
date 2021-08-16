2aa11c6caf0f4722d72eae265f42722e33ff32f4
alex <nuvoeish81@mail.ru>
2021-08-16 12:05:28 +0600

    Revert "Refresh posts media instagram url."
    This reverts commit de04cf0055b7a2151687c7329d3b4a70f2eaae90.

--- organizations/views/organization_views.py
+++ organizations/views/organization_views.py
@@ -37,9 +37,7 @@ from organizations.services.organization_services import (
     OrganizationInstagramIntegrationService
 )
 from organizations.services.subscription_services import SubscriptionService
-from organizations.tasks import (
-    parse_instagram_to_shop_items, update_media_url_by_user_entering_on_page
-)
+from organizations.tasks import parse_instagram_to_shop_items, update_videos_by_user_entering_on_page
 from users.serializers import UserShortInfoSerializer, FollowerOrClientSerializer
 
 
@@ -119,8 +117,7 @@ class OrganizationRetrieveUpdateView(RetrieveAPIView):
 
     def retrieve(self, request, *args, **kwargs):
         organization = OrganizationService.get(id=kwargs['pk'])
-        update_media_url_by_user_entering_on_page.delay(organization.pk, without_video=True)
-        update_media_url_by_user_entering_on_page.delay(organization.pk, without_video=False)
+        update_videos_by_user_entering_on_page.delay(organization.pk)
         return super().retrieve(request, *args, **kwargs)
 
 

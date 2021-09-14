31b4f4784d5ee90768bdb7cf353b568e63f513a7
Azamat Tokhtaev <krik123@gmail.com>
2021-09-14 16:25:19 +0600

    Fix instagram integration

--- users/admin.py
+++ users/admin.py
@@ -98,7 +98,7 @@ class ApofizUserAdmin(UserAdmin):
             except:
                 phone_numbers_2.append("")
             try:
-                instagrams.append(organization.instagram_integration_link.first().url)
+                instagrams.append(organization.instagram_integration.first().url)
             except:
                 instagrams.append("")
 

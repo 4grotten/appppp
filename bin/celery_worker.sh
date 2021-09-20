456cf2c9a51c4c47a964974dee726fe4443a0bdd
alex <nuvoeish81@mail.ru>
2021-09-20 10:40:38 +0600

    Bug fix - delete not updated instagram posts when user delete instagram link - returned task insta_low.

--- bin/celery_worker.sh
+++ bin/celery_worker.sh
@@ -7,4 +7,4 @@ EOF
 
 set -ex
 cd /app
-celery worker -A project --loglevel=INFO -Q high,default,insta_high,update_login_device,delete_expired_video,delete_expired_posts
+celery worker -A project --loglevel=INFO -Q high,default,insta_high,insta_low,update_login_device,delete_expired_video,delete_expired_posts
--- project/settings/base.py
+++ project/settings/base.py
@@ -282,7 +282,7 @@ CELERY_TASK_ROUTES = {
     'imagekit.cachefiles.backends._generate_file': {'queue': 'high'},
     'notifications.tasks.*': {'queue': 'default'},
     'organizations.tasks.parse_instagram_to_shop_items': {'queue': 'insta_high'},
-    # 'organizations.tasks.delete_not_updated_posts_from_instagram': {'queue': 'insta_low'},
+    'organizations.tasks.delete_not_updated_posts_from_instagram': {'queue': 'insta_low'},
     # 'organizations.tasks.delete_old_instagram_posts': {'queue': 'insta_low'},
     # 'organizations.tasks.update_instagram_videos': {'queue': 'insta_video'},
     # 'organizations.tasks.update_media_url_by_user_entering_on_page': {'queue': 'update_insta_video'},

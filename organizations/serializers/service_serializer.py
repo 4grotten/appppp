d0d5d88015aff3e3679b9db9031f0a5e366c85a7
alex <nuvoeish81@mail.ru>
2021-09-23 10:56:59 +0600

    Change service image representations.

--- organizations/serializers/service_serializer.py
+++ organizations/serializers/service_serializer.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
+from common.serializers import ImageSerializer
 from organizations.models import Service
 
 
 class ServiceSerializer(serializers.ModelSerializer):
+    icon = ImageSerializer()
 
     class Meta:
         model = Service

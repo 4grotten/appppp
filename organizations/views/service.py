82c2ecfef003cd8730a784eb759701f14d355ea5
alex <nuvoeish81@mail.ru>
2021-09-24 19:21:57 +0600

    Added filtering services by country.

--- organizations/views/service.py
+++ organizations/views/service.py
@@ -1,7 +1,10 @@
+from django.utils.translation import gettext_lazy as _
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from organizations.models import Service
+from common.exceptions import NotAcceptableException
+from organizations.models import Service, Organization
+from organizations.serializers.query_param_serializers import OrganizationCoutrySerializer
 from organizations.serializers.service_serializers import ServiceSerializer
 
 
@@ -10,3 +13,17 @@ class ServiceReadOnlySet(viewsets.ReadOnlyModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
     pagination_class = None
+
+    def get_queryset(self):
+        serializer = OrganizationCoutrySerializer(data=self.request.GET)
+        if not serializer.is_valid():
+            raise NotAcceptableException(
+                _('Valid country and city are required in query parameters'))
+
+        country = serializer.validated_data['country']
+        city = serializer.validated_data['city']
+
+        organizations_in_country = Organization.objects.filter(country=country,
+                                                               city=city).values_list('types', flat=True)
+        queryset = Service.objects.filter(subcategory__in=organizations_in_country).distinct()
+        return queryset

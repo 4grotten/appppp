ab6086fd1c0333843088c0831eefd2b17014d91d
alex <nuvoeish81@mail.ru>
2021-09-24 20:39:36 +0600

    2

--- organizations/views/service.py
+++ organizations/views/service.py
@@ -6,7 +6,6 @@ from common.exceptions import NotAcceptableException
 from organizations.models import Service, Organization
 from organizations.serializers.query_param_serializers import OrganizationCoutrySerializer
 from organizations.serializers.service_serializers import ServiceSerializer
-from organizations.services.organization_services import OrganizationService
 
 
 class ServiceReadOnlySet(viewsets.ReadOnlyModelViewSet):
@@ -25,8 +24,14 @@ class ServiceReadOnlySet(viewsets.ReadOnlyModelViewSet):
         print(country)
         city = serializer.validated_data['city']
 
-        organizations = Organization.objects.all()
-        organizations_in_country = OrganizationService._filter_by_country_and_city(queryset=organizations, country=country, city=city).distinct().values_list('types', flat=True)
+        queryset = Organization.objects.all()
+        print(queryset.count())
+        if country is not None:
+            queryset = queryset.filter(country=country)
+        if city is not None:
+            queryset = queryset.filter(city=city)
+        print(queryset.count())
+        organizations_in_country = queryset.values_list('types', flat=True).distinct()
         print(organizations_in_country)
         services = Service.objects.filter(subcategory__in=organizations_in_country).distinct()
         return services

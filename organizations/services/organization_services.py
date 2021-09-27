61939e63dd64837aab2874c8061f293f58e11114
alex <nuvoeish81@mail.ru>
2021-09-27 18:10:12 +0600

    Exclude is_banned and is_deleted organizations from service list.

--- organizations/services/organization_services.py
+++ organizations/services/organization_services.py
@@ -439,7 +439,8 @@ class OrganizationService:
                                       city: Union[City, None] = None) -> QuerySet:
         queryset = Organization.objects.filter(is_active=True, types__in=service.subcategory.all(),
                                                shop_items__isnull=False, shop_items__price__isnull=False
-                                               ).order_by('opens_at', '-closes_at')
+                                               ).exclude(is_banned=True).exclude(is_deleted=True).order_by('opens_at',
+                                                                                                           '-closes_at')
         queryset = cls._filter_by_country_and_city(queryset=queryset, country=country, city=city).distinct()
         return queryset
 

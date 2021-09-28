4dad896e24ce041055a13ec0bd784c255f4a83a3
alex <nuvoeish81@mail.ru>
2021-09-28 16:38:08 +0600

    Bug fixed - required query parameters can be omitted.

--- organizations/serializers/query_param_serializers.py
+++ organizations/serializers/query_param_serializers.py
@@ -22,7 +22,7 @@ class OrganizationAndCategorySerializer(serializers.Serializer):
 class OrganizationCoutrySerializer(serializers.Serializer):
     country = serializers.PrimaryKeyRelatedField(queryset=Country.objects.all(), default=None)
     city = serializers.PrimaryKeyRelatedField(queryset=City.objects.all(), default=None)
-    current_timestamp_lt = serializers.DateTimeField()
+    current_timestamp_lt = serializers.DateTimeField(default=None)
 
 
 class OrganizationTransactionsQueryParamSerializer(serializers.Serializer):

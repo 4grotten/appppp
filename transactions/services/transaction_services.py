04274b24e00b09a2e1efc0c40747ac22c6f076a6
Azamat Tokhtaev <krik123@gmail.com>
2021-10-03 12:04:10 +0600

    Fix viewing transaction for delivery services

--- transactions/services/transaction_services.py
+++ transactions/services/transaction_services.py
@@ -68,9 +68,9 @@ class TransactionService:
     def get_transaction(cls, transaction_id: int, requested_by: User) -> Transaction:
         instance = cls.get(id=transaction_id)
 
-        if instance.client != requested_by and not OrganizationService.user_can_see_stats(
-                organization=instance.organization, user=requested_by):
-            raise PermissionDeniedException(_('Permission denied'))
+        # if instance.client != requested_by and not OrganizationService.user_can_see_stats(
+        #         organization=instance.organization, user=requested_by):
+        #     raise PermissionDeniedException(_('Permission denied'))
 
         return instance
 

3a31dc4095f1cff936c89cd47c1f55459cf0cd6d
alex <nuvoeish81@mail.ru>
2021-10-08 17:03:40 +0600

    Endpoint to get ip data from client throw nginx.

--- cors/views.py
+++ cors/views.py
@@ -7,6 +7,7 @@ from django.utils.translation import gettext_lazy as _
 from django.views import View
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from instagram_parsers.services.proxy_services import ProxyService
 
@@ -41,23 +42,20 @@ class CorsView(View):
             return Response(data={f"Error": f"{str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
 
 
-class IpLocation(View):
+class IpLocation(APIView):
 
     def get(self, request):
-
-        print(request.META.get('X_FORWARDED_FOR'))
-        print(request.META.get('REMOTE_ADDR'))
-        x_f = request.META.get('X_FORWARDED_FOR')
-        r_a = request.META.get('REMOTE_ADDR')
-        i, r = get_client_ip(request, request_header_order=['X_FORWARDED_FOR', 'REMOTE_ADDR'])
         try:
+            i, r = get_client_ip(request, request_header_order=['X_FORWARDED_FOR', 'REMOTE_ADDR'])
+            # print('IIIII', i)
+
             response = requests.get('https://geolocation-db.com/jsonp/' + f'{i}')
+
             result = response.content.decode()
             result = result.split("(")[1].strip(")")
             result = json.loads(result)
-            print(result)
+            # print(result)
 
-            return JsonResponse({'Ip': f'{i}', 'X_FORWARDED_FOR': f'{x_f}', 'REMOTE_ADDR': f'{r_a}',
-                                 'Answer': f'{response.content.decode().split("(")[1].strip(")")}'})
+            return Response(result, status=status.HTTP_200_OK)
         except Exception as e:
-            return Response(data={f"Error": f"{str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({f"Error": f"{str(e)}"}, status=status.HTTP_400_BAD_REQUEST)

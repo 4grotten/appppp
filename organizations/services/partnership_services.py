f5fa43573225da076be428cfffdddf8eec8db078
alex <nuvoeish81@mail.ru>
2021-10-25 17:01:19 +0600

    Fix 1 case - decline partnerships before accepted (second partnerships not created).     2 case - decline partnerships after accepted (second partnership already exist).

--- organizations/services/partnership_services.py
+++ organizations/services/partnership_services.py
@@ -187,8 +187,60 @@ class PartnershipService:
         except Partnership.DoesNotExist:
             pass
 
-        transaction.on_commit(lambda: Notification.objects.filter(
-            extra_data__partnership_id__in=(partnership_id, reverse_partnership.id)).delete())
+        if reverse_partnership:
+            transaction.on_commit(lambda: Notification.objects.filter(
+                extra_data__partnership_id__in=(partnership_id, reverse_partnership.id)).delete())
+
+            transaction.on_commit(lambda: send_notifications_organization_members.delay(
+                mode=NOTIFICATION_MODE_PERSONAL,
+                sender_id=user.id,
+                notification_type=NOTIFICATION_TYPE_DECLINE_PARTNERSHIP_RECIPIENT_TYPE,
+                title=PARTNERSHIP_REQUEST_TITLE.format(sender_organization=partnership.requested_by.title,
+                                                       recipient_organization=partnership.accepted_by.title),
+                description=PARTNERSHIP_REQUEST_DESCRIPTION.format(address=partnership.requested_by.address),
+                organization_id=partnership.requested_by.id,
+                members_organization_id=partnership.accepted_by.id,
+                with_permissions=dict(can_edit_partner=True),
+                extra_data=dict(partnership_id=reverse_partnership.id,
+                                sender_organization=partnership.requested_by.title,
+                                recipient_organization=partnership.accepted_by.title,
+                                address=partnership.requested_by.address),
+            ))
+
+            # # TODO пересмотреть флоу
+            try:
+                common_shop_group = CommonItemsGroup.objects.get(organizations=partnership.accepted_by)
+                partnership.accepted_by.items_group = None
+                partnership.accepted_by.save(update_fields=('items_group',))
+                count_org_in_common_group = common_shop_group.organizations.count()
+                if count_org_in_common_group == 1:
+                    partnership.requested_by.items_group = None
+                    partnership.requested_by.save(update_fields=('items_group',))
+                    common_shop_group.delete()
+            except ObjectDoesNotExist:
+                pass
+
+            reverse_partnership.delete()
+
+        else:
+            transaction.on_commit(lambda: Notification.objects.filter(extra_data__partnership_id=partnership_id).filter(
+                extra_data__should_be_deleted=True).delete())
+
+            transaction.on_commit(lambda: send_notifications_organization_members.delay(
+                mode=NOTIFICATION_MODE_PERSONAL,
+                sender_id=user.id,
+                notification_type=NOTIFICATION_TYPE_DECLINE_PARTNERSHIP_RECIPIENT_TYPE,
+                title=PARTNERSHIP_REQUEST_TITLE.format(sender_organization=partnership.requested_by.title,
+                                                       recipient_organization=partnership.accepted_by.title),
+                description=PARTNERSHIP_REQUEST_DESCRIPTION.format(address=partnership.requested_by.address),
+                organization_id=partnership.requested_by.id,
+                members_organization_id=partnership.accepted_by.id,
+                with_permissions=dict(can_edit_partner=True),
+                extra_data=dict(partnership_id=partnership.id,
+                                sender_organization=partnership.requested_by.title,
+                                recipient_organization=partnership.accepted_by.title,
+                                address=partnership.requested_by.address),
+            ))
 
         transaction.on_commit(lambda: send_notifications_organization_members.delay(
             mode=NOTIFICATION_MODE_PERSONAL,
@@ -205,37 +257,7 @@ class PartnershipService:
                             recipient_organization=partnership.accepted_by.title,
                             address=partnership.requested_by.address),
         ))
-        transaction.on_commit(lambda: send_notifications_organization_members.delay(
-            mode=NOTIFICATION_MODE_PERSONAL,
-            sender_id=user.id,
-            notification_type=NOTIFICATION_TYPE_DECLINE_PARTNERSHIP_RECIPIENT_TYPE,
-            title=PARTNERSHIP_REQUEST_TITLE.format(sender_organization=partnership.requested_by.title,
-                                                   recipient_organization=partnership.accepted_by.title),
-            description=PARTNERSHIP_REQUEST_DESCRIPTION.format(address=partnership.requested_by.address),
-            organization_id=partnership.requested_by.id,
-            members_organization_id=partnership.accepted_by.id,
-            with_permissions=dict(can_edit_partner=True),
-            extra_data=dict(partnership_id=reverse_partnership.id or partnership.id,
-                            sender_organization=partnership.requested_by.title,
-                            recipient_organization=partnership.accepted_by.title,
-                            address=partnership.requested_by.address),
-        ))
 
-        # # TODO пересмотреть флоу
-        try:
-            common_shop_group = CommonItemsGroup.objects.get(organizations=partnership.accepted_by)
-            partnership.accepted_by.items_group = None
-            partnership.accepted_by.save(update_fields=('items_group',))
-            count_org_in_common_group = common_shop_group.organizations.count()
-            if count_org_in_common_group == 1:
-                partnership.requested_by.items_group = None
-                partnership.requested_by.save(update_fields=('items_group',))
-                common_shop_group.delete()
-        except ObjectDoesNotExist:
-            pass
-
-        if reverse_partnership:
-            reverse_partnership.delete()
         partnership.delete()
 
     @classmethod

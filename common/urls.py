989a61907d8b17573029c3923f3332b977138826
alex <nuvoeish81@mail.ru>
2021-10-28 11:17:14 +0600

    Added list Languages endpoint.

--- common/urls.py
+++ common/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from common.views import (
     ImageCreateView, CountriesListView, CountryCitySearchView, WatermarkImageCreateView, ImageCreateFromUrlView,
-    YoutubeEmbedView, GetLatestAppVersion
+    YoutubeEmbedView, GetLatestAppVersion, LanguagesList
 )
 
 urlpatterns = [
@@ -11,8 +11,7 @@ urlpatterns = [
     path('watermarked_images/', WatermarkImageCreateView.as_view(), name='watermarked_images'),
     path('countries/', CountriesListView.as_view(), name='countries'),
     path('countries_and_cities/', CountryCitySearchView.as_view(), name='countries_cities_search'),
-
     path('youtube_embed/', YoutubeEmbedView.as_view(), name='youtube_embed'),
-
-    path('app_version/<slug:device>/', GetLatestAppVersion.as_view(), name='latest_app_version')
+    path('app_version/<slug:device>/', GetLatestAppVersion.as_view(), name='latest_app_version'),
+    path('languages/', LanguagesList.as_view(), name='languages_list')
 ]

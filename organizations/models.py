204b417c78f3c1f64b6bd6465f776aad0f1ae4da
emilrakaev <emilrakaev@gmail.com>
2022-03-19 16:22:22 +0600

    Make category_of_item field in Service Model not required

--- organizations/models.py
+++ organizations/models.py
@@ -529,7 +529,7 @@ class Service(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название сервиса')
     icon = models.ForeignKey('common.File', on_delete=models.SET_NULL, null=True, blank=True, related_name='services')
     subcategory = models.ManyToManyField(OrganizationType, related_name='services')
-    category_of_item = models.ManyToManyField('shop.ItemCategory', related_name='services')
+    category_of_item = models.ManyToManyField('shop.ItemCategory', related_name='services', null=True, blank=True)
     ordering = models.SmallIntegerField(verbose_name='Service ordering',
                                         validators=[MinValueValidator(1)],
                                         null=True,

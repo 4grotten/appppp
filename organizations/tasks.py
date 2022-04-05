a8fe538aaaeb3e7b0eb0ef254597fcbd1630d2e7
emilrakaev <emilrakaev@gmail.com>
2022-04-05 11:55:26 +0600

    Add save video in aws in parse_instagram_to_shop_items

--- organizations/tasks.py
+++ organizations/tasks.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.db.models import F
 from django.utils.timezone import now
 
+from common.models import File
 from instagram_parsers.models import LoginDevice
 from instagram_parsers.parsers import parser
 from organizations.constants import INSTAGRAM_POSTS_TO_PARSE
@@ -14,7 +15,6 @@ from shop.models import ShopItem, ItemInstagramData
 
 @shared_task
 def parse_instagram_to_shop_items(organization_id: int, posts_count: int = INSTAGRAM_POSTS_TO_PARSE):
-
     video_expired_time = now() + timedelta(days=settings.INSTAGRAM_VIDEO_EXPIRE_DAYS)
     mix_content_expired_time = now() + timedelta(days=settings.INSTAGRAM_IMG_EXPIRE_DAYS)
 
@@ -31,11 +31,16 @@ def parse_instagram_to_shop_items(organization_id: int, posts_count: int = INSTA
             shop_item = ShopItem.objects.create(name="Instagram", organization=organization, created_at=created_at,
                                                 updated_at=created_at, description=description, instagram_link=post_url,
                                                 )
-
             for data in instagram.get('data'):
-                ItemInstagramData.objects.create(item=shop_item,
-                                                 thumbnail_url=data.get('thumbnail_url'),
-                                                 video_url=data.get('video_url'))
+                if data.get('video_url'):
+                    video = File.objects.create(image_url=data.get('video_url'))
+                    ItemInstagramData.objects.create(item=shop_item,
+                                                     thumbnail_url=data.get('thumbnail_url'),
+                                                     video_url=video)
+                else:
+                    ItemInstagramData.objects.create(item=shop_item,
+                                                     thumbnail_url=data.get('thumbnail_url'),
+                                                     video_url=data.get('video_url'))
 
             if ShopItem.objects.filter(id=shop_item.id, instagram_data__video_url=None):
                 shop_item.removed_at = mix_content_expired_time

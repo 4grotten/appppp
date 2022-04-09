043d69514bbd704a83a666dcb33d4b1a127afa7f
emilrakaev <emilrakaev@gmail.com>
2022-04-09 17:00:16 +0600

    Create VideoFromUrlSerializer

--- common/urls.py
+++ common/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 
 from common.views import (
     ImageCreateView, CountriesListView, CountryCitySearchView, WatermarkImageCreateView, ImageCreateFromUrlView,
-    YoutubeEmbedView, GetLatestAppVersion, LanguagesList, SendEmailToApofiz, ShadowBanStatus, CurrencyConversion
+    YoutubeEmbedView, GetLatestAppVersion, LanguagesList, SendEmailToApofiz, ShadowBanStatus, CurrencyConversion,
+    VideoCreateFromUrlView
 )
 
 urlpatterns = [
     path('files/', ImageCreateView.as_view(), name='images'),
     path('save_image_from_url/', ImageCreateFromUrlView.as_view(), name='image_from_url'),
+    path('save_video_from_url/', VideoCreateFromUrlView.as_view(), name='video_from_url'),
     path('watermarked_images/', WatermarkImageCreateView.as_view(), name='watermarked_images'),
     path('countries/', CountriesListView.as_view(), name='countries'),
     path('countries_and_cities/', CountryCitySearchView.as_view(), name='countries_cities_search'),

3ea6c7e20a591b2bf2e788f1ee0f0a341b2c03cb
emilrakaev <emilrakaev@gmail.com>
2022-04-11 16:53:22 +0600

    Optimized organization update query

--- organizations/services/organization_services.py
+++ organizations/services/organization_services.py
@@ -293,7 +293,8 @@ class OrganizationService:
 
     @classmethod
     @transaction.atomic
-    def update(cls, organization, image_id, longitude, latitude, types, title, opens_at, closes_at, address, currency,
+    def update(cls, organization, image_id, longitude, latitude, types, title, opens_at, closes_at,
+               address, currency,
                show_contacts, country, is_private, avg_check=None, description=None, city=None):
         try:
             if longitude and latitude:
@@ -307,16 +308,16 @@ class OrganizationService:
             else:
                 description_lang = None
 
-            organization.title_lang = title_lang
-            organization.description_lang = description_lang
-            organization.image_id = image_id
-            organization.location = point
-            organization.title = title
-            organization.opens_at = opens_at
-            organization.closes_at = closes_at
-            organization.address = address
-            organization.avg_check = avg_check
-            organization.is_private = is_private
+            # organization.title_lang = title_lang
+            # organization.description_lang = description_lang
+            # organization.image_id = image_id
+            # organization.location = point
+            # organization.title = title
+            # organization.opens_at = opens_at
+            # organization.closes_at = closes_at
+            # organization.address = address
+            # organization.avg_check = avg_check
+            # organization.is_private = is_private
             if not organization.currency == currency:
                 from organizations.services.partnership_services import PartnershipService
                 if not PartnershipService.can_change_currency(organization=organization, currency=currency):
@@ -324,14 +325,30 @@ class OrganizationService:
 
                 from organizations.services.card_services import DiscountCardService
                 DiscountCardService.update_discount_currency(organization=organization, new_currency=currency.code)
-            organization.currency = currency
-            organization.show_contacts = show_contacts
-            organization.country = country
-            organization.city = city
-            organization.description = description
-            organization.types.set(types)
-            organization.save()
+            # organization.currency = currency
+            # organization.show_contacts = show_contacts
+            # organization.country = country
+            # organization.city = city
+            # organization.description = description
+            # organization.types.set(types)
+            # organization.save()
 
+            Organization.objects.filter(id=organization.id).update(title_lang=title_lang,
+                                                                                  description_lang=description_lang,
+                                                                                  image_id=image_id,
+                                                                                  location=point,
+                                                                                  title=title,
+                                                                                  opens_at=opens_at,
+                                                                                  closes_at=closes_at,
+                                                                                  address=address,
+                                                                                  avg_check=avg_check,
+                                                                                  is_private=is_private,
+                                                                                  currency=currency,
+                                                                                  show_contacts=show_contacts,
+                                                                                  country=country,
+                                                                                  city=city,
+                                                                                  description=description)
+            organization.types.set(types)
             return organization
 
         except Exception as e:
@@ -557,7 +574,6 @@ class OrganizationService:
     @classmethod
     def update_add_item_date(cls, user: User, instance: Organization):
         Organization.objects.filter(id=instance.id).update(add_item_date=datetime.now())
-
 
 
 class OrgPhoneNumberService:

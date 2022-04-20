fb3dcd1a879cc99ffc22beca761ba1db2f7b2719
emilrakaev <emilrakaev@gmail.com>
2022-04-20 13:20:52 +0600

    Create comment urls

--- shop/urls.py
+++ shop/urls.py
@@ -9,7 +9,8 @@ from shop.views.category_views import (
     OrganizationSubcategoryListView, NonEmptyCategoryListView, ItemCategoryRetrieveView,
     ItemCategoryAllSubcategoriesView
 )
-from shop.views.comment_views import CommentItemListView
+from shop.views.comment_views import CommentItemListCreateView, \
+    CommentDestroyUpdateRetrievtView, CommentedItemsListView, CommentLike
 from shop.views.feed_views import (
     FeedView, OrganizationItemListView, SubscriptionItemListView, HotlinkCollectionItemListView
 )
@@ -53,5 +54,9 @@ urlpatterns = [
     path('carts/<int:pk>/selfPickup/', OrderSelfPickupView.as_view(), name='order_self_pickup'),
     path('carts/<int:pk>/offline_checkout/', CartAnonymousCheckoutView.as_view(), name='cart_anonymous_checkout'),
 
-    path('comments/<int:pk>/item/', CommentItemListView.as_view(), name='comment_item_list'),
+    path('comments/item/<int:pk>/', CommentItemListCreateView.as_view(), name='comment_item_list'),
+    path('comments/<int:pk>/', CommentDestroyUpdateRetrievtView.as_view(), name='comment_retrieve'),
+    path('commented/items/', CommentedItemsListView.as_view(), name='commented_items'),
+    path('comments/like/', CommentLike.as_view(), name='comment_like'),
+
 ]

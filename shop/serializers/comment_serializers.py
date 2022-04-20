924153a4d7048691e425ffc1e3e6d790a8693c94
emilrakaev <emilrakaev@gmail.com>
2022-04-20 13:21:17 +0600

    Create comment serializers

--- shop/serializers/comment_serializers.py
+++ shop/serializers/comment_serializers.py
@@ -1,9 +1,93 @@
 from rest_framework import serializers
 
-from shop.models import Comment
+from organizations.models import Membership
+from organizations.serializers.organization_serializers import OrganizationWithImageSerializer
+from organizations.services.organization_services import OrganizationService
+from shop.models import Comment, CommentLike
+from shop.services.like_bookmark_services import LikeService
+from users.serializers import UserShortInfoSerializer
+
+
+class ParentCommentSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
+    def get_user(self, obj):
+        user = self.context['request'].user
+        if not OrganizationService.user_can_edit_organization(organization=obj.item.organization, user=user):
+            if OrganizationService.user_can_edit_organization(organization=obj.item.organization, user=obj.user):
+                return OrganizationWithImageSerializer(obj.item.organization).data
+            return UserShortInfoSerializer(obj.user).data
+        return UserShortInfoSerializer(obj.user).data
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'user', 'text')
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    is_comment_liked = serializers.SerializerMethodField()
+    permissions = serializers.SerializerMethodField()
+    user = serializers.SerializerMethodField()
+    user_role = serializers.SerializerMethodField()
+    comment_like_count = serializers.SerializerMethodField()
+    can_delete = serializers.SerializerMethodField()
+    parent = ParentCommentSerializer()
+
+    def get_can_delete(self, obj) -> bool:
+        user = self.context['request'].user
+        if OrganizationService.user_can_edit_organization(organization=obj.item.organization, user=user) or \
+                user == obj.user:
+            return True
+        return False
+
+    def get_permissions(self, obj):
+        return OrganizationService.get_user_permissions_dict(organization=obj.item.organization,
+                                                             user=obj.user)
+
+    def get_user(self, obj):
+        user = self.context['request'].user
+        if not OrganizationService.user_can_edit_organization(organization=obj.item.organization, user=user):
+            if OrganizationService.user_can_edit_organization(organization=obj.item.organization, user=obj.user):
+                return OrganizationWithImageSerializer(obj.item.organization).data
+            return UserShortInfoSerializer(obj.user).data
+        return UserShortInfoSerializer(obj.user).data
+
+    def get_user_role(self, obj):
+        try:
+            membership = Membership.objects.get(organization=obj.item.organization, user=obj.user)
+            return membership.role.title
+        except Membership.DoesNotExist:
+            return None
+
+    def get_is_comment_liked(self, comment: Comment) -> bool:
+        user = self.context['request'].user
+        if not user.is_authenticated:
+            return False
+        return LikeService.is_comment_liked_by_user(comment=comment, user=user)
+
+    def get_comment_like_count(self, comment: Comment) -> int:
+        return comment.liked_comments.count()
+
     class Meta:
         model = Comment
-        fields = ('user', 'item', 'parent', 'text')
+        fields = (
+            'id', 'user', 'item', 'parent', 'text', 'user_role', 'is_comment_liked', 'comment_like_count', 'can_delete',
+            'permissions')
+
+
+class CommentLikeSerializer(serializers.ModelSerializer):
+    is_liked = serializers.BooleanField()
+
+    class Meta:
+        model = CommentLike
+        fields = ('comment', 'is_liked')
+
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('item', 'parent', 'text')
+
+    def validate(self, attrs):
+        attrs['user'] = self.context['request'].user
+        return attrs

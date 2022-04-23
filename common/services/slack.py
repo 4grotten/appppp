73f667d632372ae893d88b5bf0c9af9469ec16e1
emilrakaev <emilrakaev@gmail.com>
2022-04-23 19:02:14 +0600

    Add new slack chanel and add output errors

--- common/services/slack.py
+++ common/services/slack.py
@@ -16,6 +16,23 @@ def bot(message):
     data = {
         "channel": f"{channel}",
         "text": f"{message}"
-        }
+    }
+    data = json.dumps(data)
+    requests.post(url=slack_url, data=data, headers=headers)
+
+
+def bot_2(message):
+    slack_token = settings.SLACK_TOKEN
+    slack_url = 'https://slack.com/api/chat.postMessage'
+    channel = "C03CFMZDZ1B"
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {slack_token}'
+    }
+
+    data = {
+        "channel": f"{channel}",
+        "text": f"{message}"
+    }
     data = json.dumps(data)
     requests.post(url=slack_url, data=data, headers=headers)

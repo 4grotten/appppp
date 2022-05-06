5c0d72ac4fb3761b09f64d05da1fb2e52ad86440
emilrakaev <emilrakaev@gmail.com>
2022-05-06 11:21:32 +0600

    Add item field in Notification model and makemigration

--- notifications/models.py
+++ notifications/models.py
@@ -30,6 +30,9 @@ class Notification(TimestampModel):
     is_read = models.BooleanField(default=False)
     organization = models.ForeignKey('organizations.Organization', on_delete=models.SET_NULL, blank=True, null=True,
                                      related_name='organization_notifications')
+
+    item = models.ForeignKey('shop.ShopItem', on_delete=models.SET_NULL, blank=True, null=True,
+                             related_name='shopitem_notifications')
     mode = models.CharField(max_length=255, choices=NOTIFICATION_MODES)
     type = models.CharField(max_length=255, choices=NOTIFICATION_TYPES, default=SYSTEM_TYPE)
     extra_data = models.JSONField(null=True)

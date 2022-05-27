14a329cf8732c562b0792f5298031364647637ff
emilrakaev <emilrakaev@gmail.com>
2022-05-27 17:00:12 +0600

    Changed organization search from elastic to database search

--- search_indexes/urls.py
+++ search_indexes/urls.py
@@ -6,7 +6,7 @@ from search_indexes.views.item_views import SuggestSearchDocumentView
 from search_indexes.views.organization_views import OrganizationDocumentView
 
 router = DefaultRouter()
-router.register('homepage/search', OrganizationDocumentView, basename='homepage_search')
+# router.register('homepage/search', OrganizationDocumentView, basename='homepage_search')
 # router.register('search/item', SuggestSearchDocumentView, basename='search/item')
 # router.register('shop/organization_items', ShopOrgnizationItemDocumentView, basename='organization_items')
 # router.register('shop/feed', ShopItemDocumentView, basename='shop_feed')

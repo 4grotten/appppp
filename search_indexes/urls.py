6b7d46d00b1ad5443bec038cc94b54fa2bdb21fc
emilrakaev <emilrakaev@gmail.com>
2022-05-27 15:40:21 +0600

    Comment elastic suggest item url

--- search_indexes/urls.py
+++ search_indexes/urls.py
@@ -7,7 +7,7 @@ from search_indexes.views.organization_views import OrganizationDocumentView
 
 router = DefaultRouter()
 router.register('homepage/search', OrganizationDocumentView, basename='homepage_search')
-router.register('search/item', SuggestSearchDocumentView, basename='search/item')
+# router.register('search/item', SuggestSearchDocumentView, basename='search/item')
 # router.register('shop/organization_items', ShopOrgnizationItemDocumentView, basename='organization_items')
 # router.register('shop/feed', ShopItemDocumentView, basename='shop_feed')
 

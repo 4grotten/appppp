a4663e9b2e60f81a2af4c89f185b230742701acc
emilrakaev <emilrakaev@gmail.com>
2022-06-20 21:02:43 +0600

    Add script for fill content White Cat organization

--- stock/views.py
+++ stock/views.py
@@ -1,9 +1,14 @@
+import json
+
 from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 
+from common.models import File
+from organizations.models import Organization
+from shop.models import ItemCategory, ItemSubcategory, ShopItem
 from stock.serializers import FormatCriteriaSerializer, SizeFormatSerializer, CriteriaSubcategorySerializer
 from stock.services import StockService
 
@@ -29,4 +34,22 @@ class SizeByFormatListView(ListAPIView):
     serializer_class = SizeFormatSerializer
 
     def get_queryset(self):
+
+        org = Organization.objects.get(title='Белый Кот')
+        category = ItemCategory.objects.get(name='House')
+        with open('./shop/migrations/myfile.json', 'r') as f:
+            data = json.load(f)
+        for i in data:
+            subcategory, _ = ItemSubcategory.objects.get_or_create(category=category, name=str(i['category']),
+                                                                   organization=org)
+            images_list = []
+
+            for j in range(len(i['images'])):
+                file = File.objects.create(image_url=str(i['images'][j]), is_watermarked=True)
+                images_list.append(file)
+            item, _ = ShopItem.objects.get_or_create(organization=org, subcategory=subcategory, name=i['name'],
+                                                     description=i['description'], price=i['price'],
+                                                     youtube_links=i['video'])
+            item.images.add(*images_list)
+
         return StockService.get_sizes_by_format_id(self.kwargs['pk'])

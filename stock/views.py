5862d75a46186e97646376e33dc974b3e7a84423
emilrakaev <emilrakaev@gmail.com>
2022-06-20 21:56:08 +0600

    Comment script for fill content White Cat organization

--- stock/views.py
+++ stock/views.py
@@ -26,9 +26,9 @@ class FormatCriteriaListView(ListAPIView):
     serializer_class = FormatCriteriaSerializer
 
     def get_queryset(self):
-        org = Organization.objects.get(title='Белый Кот')
-        ShopItem.objects.filter(organization=org).delete()
-        ItemSubcategory.objects.filter(organization=org).delete()
+        # org = Organization.objects.get(title='Белый Кот')
+        # ShopItem.objects.filter(organization=org).delete()
+        # ItemSubcategory.objects.filter(organization=org).delete()
         return StockService.get_format_by_criteria_subcategory_id(self.kwargs['pk'])
 
 
@@ -38,23 +38,23 @@ class SizeByFormatListView(ListAPIView):
 
     def get_queryset(self):
 
-        org = Organization.objects.get(title='Белый Кот')
-        category = ItemCategory.objects.get(name='House')
-        with open('./shop/migrations/myfile.json', 'r') as f:
-            data = json.load(f)
-        for i in data:
-            if not ShopItem.objects.filter(name=i['name'], description=i['description'], organization=org).exists():
-                subcategory, _ = ItemSubcategory.objects.get_or_create(category=category, name=str(i['category']),
-                                                                       organization=org)
-                print(subcategory)
-                images_list = []
-
-                item, _ = ShopItem.objects.get_or_create(organization=org, subcategory=subcategory, name=i['name'],
-                                                         description=i['description'], price=i['price'],
-                                                         youtube_links=i['video'])
-                for j in range(len(i['images'])):
-                    file = File.objects.create(image_url=str(i['images'][j]), is_watermarked=True)
-                    images_list.append(file)
-                item.images.add(*images_list)
+        # org = Organization.objects.get(title='Белый Кот')
+        # category = ItemCategory.objects.get(name='House')
+        # with open('./shop/migrations/myfile.json', 'r') as f:
+        #     data = json.load(f)
+        # for i in data:
+        #     if not ShopItem.objects.filter(name=i['name'], description=i['description'], organization=org).exists():
+        #         subcategory, _ = ItemSubcategory.objects.get_or_create(category=category, name=str(i['category']),
+        #                                                                organization=org)
+        #         print(subcategory)
+        #         images_list = []
+        #
+        #         item, _ = ShopItem.objects.get_or_create(organization=org, subcategory=subcategory, name=i['name'],
+        #                                                  description=i['description'], price=i['price'],
+        #                                                  youtube_links=i['video'])
+        #         for j in range(len(i['images'])):
+        #             file = File.objects.create(image_url=str(i['images'][j]), is_watermarked=True)
+        #             images_list.append(file)
+        #         item.images.add(*images_list)
 
         return StockService.get_sizes_by_format_id(self.kwargs['pk'])

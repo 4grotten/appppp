9da8b2cad6e5cfa4d7ac8dd352b0ea5fd6008525
emilrakaev <emilrakaev@gmail.com>
2022-07-08 21:08:42 +0600

    Fixed delete stock

--- stock/views.py
+++ stock/views.py
@@ -255,6 +255,7 @@ class DeleteStockView(DestroyAPIView):
     def destroy(self, request, *args, **kwargs):
         shop_item = ShopItemService.get(id=kwargs['pk'])
         ShopItemSizeCount.objects.filter(main_shop_item=shop_item).delete()
+        shop_item.available_sizes.clear()
 
         return Response(data={
             'message': _('Successfully deleted'),

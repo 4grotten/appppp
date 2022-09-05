c7cffb6b8cbb870254cd00d9f489afa8dd732ddd
ML500 <emilrakaev@gmail.com>
2022-09-05 21:58:15 +0600

    Refactor code with ordering search priority

--- shop/services/item_services.py
+++ shop/services/item_services.py
@@ -162,12 +162,12 @@ class ShopItemService:
     @classmethod
     def get_ordering_search_result(cls, queryset: QuerySet, search_word: str) -> QuerySet:
         queryset = queryset.annotate(name_order=Case(
-            When(name__iexact=search_word, then=0),
-            When(name__in=search_word.split(), then=1),
-            When(name__icontains=search_word, then=2),
-            When(description__icontains=search_word, then=3),
-            When(article__icontains=search_word, then=4),
-            default=Value(5),
+            When(name__iexact=search_word, then=1),
+            When(name__in=search_word.split(), then=2),
+            When(name__icontains=search_word, then=3),
+            When(description__icontains=search_word, then=4),
+            When(article__icontains=search_word, then=5),
+            default=Value(6),
             output_field=IntegerField(),
         )).order_by('name_order', '-updated_at', )
 

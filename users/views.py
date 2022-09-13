aaccec24cdbdec4a15909eb12ab9fb42b3145a0d
big-arturka <arturkrmnlv10@gmail.com>
2022-09-13 17:13:16 +0600

    Revert "Add google recaptcha"
    This reverts commit 80780bbc13cfac9e1900effc8e5f042f79781b9b.

--- users/views.py
+++ users/views.py
@@ -3,7 +3,6 @@ from django.db.models.query_utils import Q
 from django.utils.translation import gettext_lazy
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from rest_framework.exceptions import Throttled
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -25,19 +24,11 @@ from .serializers import (
 from .services import (
     UserService, TemporaryCodeService, PhoneNumberService, SocialNetworkContactService, TemporaryPhoneNumberService
 )
-from .throttle.throttle import UserLoginRateThrottle
 
 
 class RegisterAuthAPIView(APIView):
     permission_classes = ()
     authentication_classes = ()
-    throttle_classes = (UserLoginRateThrottle,)
-
-    def throttled(self, request, wait):
-        print(request.data)
-        raise Throttled(detail={
-            "message": "recaptcha_required",
-        })
 
     def post(self, request):
         serializer = RegisterAuthSerializer(data=request.data)

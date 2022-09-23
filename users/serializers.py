ac375472c34b4eb0627fb1490590f356c368a2f7
big-arturka <arturkrmnlv10@gmail.com>
2022-09-23 13:20:55 +0600

    Add operating_system to serializers

--- users/serializers.py
+++ users/serializers.py
@@ -284,4 +284,4 @@ class UserWhitClientOrRoleInfoSerializer(serializers.ModelSerializer):
 class MyOwnTokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyOwnToken
-        fields = ('id', 'key', 'user', 'location', 'device', 'ip', 'log_time', 'version_app','is_active')
+        fields = ('id', 'key', 'user', 'location', 'device', 'ip', 'log_time', 'version_app','is_active', 'operating_system')

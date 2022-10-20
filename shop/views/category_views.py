3cc1ad18546f58b9557a961469904f70a8ce81d5
big-arturka <arturkrmnlv10@gmail.com>
2022-10-20 14:09:59 +0600

    Fixed non empty categories

--- shop/views/category_views.py
+++ shop/views/category_views.py
@@ -86,7 +86,7 @@ class NonEmptyPartnerCategoryListView(ListAPIView):
     def get_queryset(self):
         main_organization = Organization.objects.get(id=self.kwargs['pk'])
         partners = main_organization.requested_partnerships.filter(is_accepted=True).values_list('accepted_by', flat=True).distinct()
-        partner_organizations = Organization.objects.filter(Q(id__in=partners) | Q(id=self.kwargs['pk']))
+        partner_organizations = Organization.objects.filter(Q(id__in=partners) | Q(id=self.kwargs['pk']) & Q(is_active=True, is_private=False, is_banned=False))
         item_categories = ShopItem.objects.filter(organization__in=partner_organizations, is_published=True, is_hidden=False).values_list('subcategory_id', flat=True).distinct()
         return ItemCategory.objects.filter(subcategories__in=item_categories).distinct().order_by('name')
 

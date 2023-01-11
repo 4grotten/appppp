a0b3db2fb4fe16271ce24a03ddbc403763021d62
big-arturka <arturkrmnlv10@gmail.com>
2023-01-11 15:01:29 +0600

    fix: change back to jpeg image

--- common/models.py
+++ common/models.py
@@ -13,7 +13,6 @@ from imagekit.models import ImageSpecField
 from common.constants import DEVICE_TYPES, MESSAGE_TYPE
 from common.processors import ResizeWatermarkedSpec, MobileWallpaper
 from common.utils import upload_file_with_unique_name, upload_file_video_with_unique_name
-from django_resized import ResizedImageField
 
 
 class LargeWatermarkedSpec(ResizeWatermarkedSpec):
@@ -50,8 +49,7 @@ class File(TimestampModel):
     is_watermarked = models.BooleanField(default=False, editable=False)
     order = models.PositiveSmallIntegerField(default=0, editable=False)
 
-    file = ResizedImageField(
-        force_format="WEBP", quality=75,
+    file = models.ImageField(
         upload_to=upload_file_with_unique_name,
         help_text=_('Image that you want to store'),
         max_length=1000

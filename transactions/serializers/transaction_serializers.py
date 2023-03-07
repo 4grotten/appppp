b42c637d2005d6367dc2214ed2a60bffa38c9da1
Erzhan <erzhanmuratov99@gmail.com>
2023-03-07 17:42:50 +0600

    fix: add status in transaction

--- transactions/serializers/transaction_serializers.py
+++ transactions/serializers/transaction_serializers.py
@@ -243,7 +243,7 @@ class BookingTransactionWithClientSerializer(TransactionDetailSerializer):
             'id', 'purchase_id', 'currency', 'original_amount', 'discount_percent', 'savings', 'from_cashback',
             'to_cashback', 'final_amount', 'processed_by', 'employee_name', 'employee_avatar', 'employee_role',
             'updated_at', 'created_at', 'display_time', 'client', 'type', 'current_user_can_see_stats', 'organization',
-            'booking'
+            'booking', 'status'
         )
 
 

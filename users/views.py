faebee7fb91f0fcee47c33a4175d1ce875c2a8af
Erzhan <erzhanmuratov99@gmail.com>
2023-05-22 22:46:28 +0600

    fix: use get method for token

--- users/views.py
+++ users/views.py
@@ -128,9 +128,7 @@ class VerifyTemporaryCodeAPIView(APIView):
         user = UserService.get(phone_number=phone_number)
 
         try:
-            token = MyOwnToken.objects.filter(user=user,
-                                           is_active=True,
-                                           ip=request.META.get('REMOTE_ADDR')).order_by('-log_time').first()
+            token = MyOwnToken.objects.get(user=user, is_active=True, ip=request.META.get('REMOTE_ADDR'))
         except MyOwnToken.DoesNotExist:
             token = MyOwnToken.objects.create(user=user, ip=request.META.get('REMOTE_ADDR'))
             token.save()

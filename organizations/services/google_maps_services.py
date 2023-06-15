ff482b824da016279e1fef30ff99b6105d323ee9
Erzhan <erzhanmuratov99@gmail.com>
2023-06-15 18:56:27 +0600

    fix: fix the get_place_CID method

--- organizations/services/google_maps_services.py
+++ organizations/services/google_maps_services.py
@@ -27,9 +27,29 @@ class GoogleMapsService:
 
     @classmethod
     def get_place_CID(cls, gMaps_URL) -> str:
-        page_source = requests.get(gMaps_URL).text
-        cid_match = re.search(r'ludocid\\\\u003d(.*?)\\\\u00', page_source)
-        if cid_match is None:
+        try:
+            if "goo.gl/maps" in gMaps_URL:
+                # Web version URL
+                text = requests.get(gMaps_URL).url
+                feature_id = re.search(r':(0x[a-z0-9]+)!', text).group(1)
+                cid_hexadecimal = feature_id.split(':')[-1]
+                cid = str(int(cid_hexadecimal, 16))
+                return cid
+            elif "maps.app.goo.gl" in gMaps_URL and "?" in gMaps_URL:
+                # iOS version URL
+                text = requests.get(gMaps_URL).url
+                feature_id = re.search(r'tid=(.*?)&hl=', text).group(1)
+                cid_hexadecimal = feature_id.split(':')[-1]
+                cid = str(int(cid_hexadecimal, 16))
+                return cid
+            else:
+                # Android version URL
+                text = requests.get(gMaps_URL).url
+                feature_id = re.search(r':(0x[a-z0-9]+)\?utm_source=', text).group(1)
+                cid_hexadecimal = feature_id.split(':')[-1]
+                cid = str(int(cid_hexadecimal, 16))
+                return cid
+        except:
             error_data = {
                 "message": "Invalid input",
                 "errors": {
@@ -39,9 +59,6 @@ class GoogleMapsService:
                 }
             }
             raise ValidationError(error_data)
-        cid = cid_match.group(1).strip('\\')
-
-        return cid
 
 
     @classmethod

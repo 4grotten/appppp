ff8b55f1cee58f3bc62dc84574ffa16ddac3325f
Erzhan <erzhanmuratov99@gmail.com>
2023-06-15 18:06:46 +0600

    fix: change back to first way of getting place cid but without header

--- organizations/services/google_maps_services.py
+++ organizations/services/google_maps_services.py
@@ -27,12 +27,9 @@ class GoogleMapsService:
 
     @classmethod
     def get_place_CID(cls, gMaps_URL) -> str:
-        try:
-            page_source = requests.get(gMaps_URL, headers=HEADERS).text
-            cid = re.search(r'ludocid\\\\u003d(.*?)\\\\u00', page_source).group(1).strip('\\')
-
-            return cid
-        except:
+        page_source = requests.get(gMaps_URL).text
+        cid_match = re.search(r'ludocid\\\\u003d(.*?)\\\\u00', page_source)
+        if cid_match is None:
             error_data = {
                 "message": "Invalid input",
                 "errors": {
@@ -42,6 +39,10 @@ class GoogleMapsService:
                 }
             }
             raise ValidationError(error_data)
+        cid = cid_match.group(1).strip('\\')
+
+        return cid
+
 
     @classmethod
     def get_place_details(cls, gMaps_URL: str) -> Dict:

6c8414613064baef31568d9f05c82512b596f102
Erzhan <erzhanmuratov99@gmail.com>
2023-09-11 15:58:51 +0600

    fix: clear debug currency

--- common/services/currency.py
+++ common/services/currency.py
@@ -16,7 +16,6 @@ class CurrencyConverterService:
     def made_rate_request(cls, app_id, from_currency, to_currency):
         query = f'app_id={app_id}&symbols={from_currency},{to_currency}'
         response = requests.get(f'https://openexchangerates.org/api/latest.json?{query}')
-        print("TY DALBI4?")
         if response.status_code != status.HTTP_200_OK:
             raise NotAcceptableException(_('Bad response from openexchangerates.org'))
         return response
@@ -36,25 +35,20 @@ class CurrencyConverterService:
             from_rate_to_base = response.json()['rates'][from_currency.upper()]
         except KeyError:
             raise NotAcceptableException(_('No currency with code %(currency)s') % {'currency': from_currency.upper()})
-        print("from_rate_to_base", from_rate_to_base)
         cache.set(f'{settings.OER_BASE_CURRENCY}{from_currency}', from_rate_to_base, timeout=settings.OER_CACHE_TIMEOUT)
-        print("CACHED!")
         try:
             to_rate_to_base = response.json()['rates'][to_currency.upper()]
         except KeyError:
             raise NotAcceptableException(_(f'No currency with code {to_currency.upper()}'))
         cache.set(f'{settings.OER_BASE_CURRENCY}{to_currency}', to_rate_to_base, timeout=settings.OER_CACHE_TIMEOUT)
-        print("CACHED2!")
 
         rate = Decimal(to_rate_to_base) / Decimal(from_rate_to_base)
         cache.set(f'{from_currency}{to_currency}', rate, timeout=settings.OER_CACHE_TIMEOUT)
-        print("CACHED3!")
 
         return rate
 
     @classmethod
     def get_rate_from_cache(cls, from_currency: str, to_currency: str):
-        print("URA!")
         cached_exchange_rate = cache.get(f'{from_currency}{to_currency}')
         if cached_exchange_rate:
             return cached_exchange_rate

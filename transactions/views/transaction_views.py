a509ae8523302533a801e754224522b475f0d5bf
Erzhan <erzhanmuratov99@gmail.com>
2023-10-05 10:29:30 +0600

    refactor: delete unnecessary print statements

--- transactions/views/transaction_views.py
+++ transactions/views/transaction_views.py
@@ -1382,7 +1382,6 @@ class InitPaymentView(GenericAPIView):
             'user_id': str(self.request.user.id),
             'purchase_type': purchase_type
         }
-        print(payment_data)
 
 
         request_for_signature = TransactionService.make_flat_params_array(payment_data)
@@ -1390,14 +1389,11 @@ class InitPaymentView(GenericAPIView):
         signature_params = ['init_payment.php'] + [str(value) for _, value in sorted_params] + [FREEDOMPAY_RECEIVE_SECRET]
         signature = hashlib.md5(';'.join(signature_params).encode()).hexdigest()
         payment_data['pg_sig'] = signature
-        print("BEFORE REQUEST")
         response = requests.post('https://api.freedompay.money/init_payment.php', data=payment_data)
-        print("AFTER REQUEST")
         xml_data = response.text
         response_dict = xmltodict.parse(xml_data)
         json_string = json.dumps(response_dict)
         json_data = json.loads(json_string)
-        print("AFTER JSON", json_data)
 
         return Response(json_data, content_type='application/json')
 

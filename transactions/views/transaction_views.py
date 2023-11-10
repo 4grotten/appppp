b7c0f670c7b98ce85c63b448529652695106da10
Erzhan <erzhanmuratov99@gmail.com>
2023-11-10 12:58:12 +0600

    feat: add pagination TransactionOwnTicketInfoView

--- transactions/views/transaction_views.py
+++ transactions/views/transaction_views.py
@@ -12,12 +12,12 @@ from django.utils.translation import gettext_lazy as _
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, filters
 from rest_framework.filters import SearchFilter
-from rest_framework.generics import GenericAPIView, ListAPIView, RetrieveDestroyAPIView, RetrieveAPIView
+from rest_framework.generics import GenericAPIView, ListAPIView, RetrieveDestroyAPIView, RetrieveAPIView, CreateAPIView
+from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from common.models import Currency
 from common.services.currency import CurrencyConverterService
 from project.settings.base import FREEDOMPAY_PROJECT_ID, FREEDOMPAY_RECEIVE_SECRET, FREEDOMPAY_PAYOUT_SECRET, \
     PAYSY_API_KEY
@@ -1314,7 +1314,7 @@ class TransactionTicketUserInfoView(GenericAPIView):
         return self.get_paginated_response(response_data)
 
 
-class TransactionOwnTicketInfoView(GenericAPIView):
+class TransactionOwnTicketInfoView(ListAPIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = IsActiveTicketSerializer
 
@@ -1328,11 +1328,15 @@ class TransactionOwnTicketInfoView(GenericAPIView):
 
         ticket = serializer.validated_data['item']
 
-        tickets = Ticket.objects.filter(item=ticket, user=request.user).order_by('-updated_at')
+        queryset = Ticket.objects.filter(item=ticket, user=request.user).order_by('-updated_at')
 
-        serializer = self.get_serializer(tickets, many=True)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
 
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 class TransactionTicketInfoView(GenericAPIView):
     permission_classes = (IsAuthenticated,)

af44871877b3ad5d0ffa8111113f47f0a26c67f4
Erzhan <erzhanmuratov99@gmail.com>
2023-11-22 10:48:07 +0600

    fix: delete subcategories from OrganizationMapsListSerializer

--- organizations/serializers/organization_serializers.py
+++ organizations/serializers/organization_serializers.py
@@ -420,16 +420,12 @@ class OrganizationListSerializer(serializers.ModelSerializer):
 
 class OrganizationMapsListSerializer(serializers.ModelSerializer):
     image = ImageSerializer(many=False)
-    subcategories = serializers.SerializerMethodField()
 
     class Meta:
         model = Organization
         fields = (
-            'id', 'title', 'image', 'avg_check', 'currency', 'full_location', 'types', 'subcategories')
+            'id', 'title', 'image', 'avg_check', 'currency', 'full_location', 'types')
         read_only_fields = ['verification_status']
-
-    def get_subcategories(self, obj):
-        return [item.subcategory.id for item in obj.shop_items.all() if item.subcategory]
 
 
 class OrganizationCreateSerializer(serializers.ModelSerializer):

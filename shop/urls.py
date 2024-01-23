3cbcc31df54c7375be15f3e42144beeeb697ff70
Erzhan <erzhanmuratov99@gmail.com>
2024-01-23 14:41:07 +0600

    feat: add OrganizationDeclineResumeRequestView

--- shop/urls.py
+++ shop/urls.py
@@ -29,7 +29,7 @@ from shop.views.item_views import (
     ResumeWorkExperienceListView, ResumeWorkExperienceUpdateView, ResumeInfoRetrieveAPIView, EducationListView,
     ResumeEducationListView, ResumeEducationUpdateView, SubmitResumeRequestView, AcceptResumeRequestView,
     DeclineResumeRequestView, UserResumeRequestRetrieveView, OrganizationSubmitResumeRequestView,
-    OrganizationResumeRequestRetrieveView, OrganizationAcceptResumeRequestView
+    OrganizationResumeRequestRetrieveView, OrganizationAcceptResumeRequestView, OrganizationDeclineResumeRequestView
 )
 
 urlpatterns = [
@@ -105,6 +105,7 @@ urlpatterns = [
     path('shop/resume/organization/request/<int:pk>/', OrganizationResumeRequestRetrieveView.as_view(),
          name='resume_user_request_retrieve'),
     path('shop/resume/organization/accept/', OrganizationAcceptResumeRequestView.as_view(), name='resume_user_accept'),
+    path('shop/resume/organization/decline/', OrganizationDeclineResumeRequestView.as_view(), name='resume_user_decline'),
 
     path('shop/feed/', FeedView.as_view(), name='shop_feed'),
     path('search/item/', SuggestSearchItem.as_view(), name='suggest_item'),

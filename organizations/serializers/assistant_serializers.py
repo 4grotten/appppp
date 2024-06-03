8ac6773e9b1f3da7129404ad0ce705f11972d229
Erzhan <erzhanmuratov99@gmail.com>
2024-06-03 19:20:16 +0600

    feat: add OrganizationAssistantCreateView, QuestionListView, OrganizationAssistantAnswerCreateView, OrganizationAssistantAnswerRetrieveUpdateView, AnswerFileCreateView

---
+++ organizations/serializers/assistant_serializers.py
@@ -0,0 +1,95 @@
+from rest_framework import serializers
+from django.utils.translation import gettext_lazy as _
+
+from common.models import File
+from organizations.models import Assistant, Organization, Answer, AnswerFile, Question
+from organizations.services.assistant_services import AssistantService
+
+
+class AnswerFileSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = AnswerFile
+        fields = ('id', 'file', 'name')
+        read_only_fields = ('name',)
+
+    def get_name(self, obj):
+        return obj.file.name.split("/")[-1]
+
+
+class AssistantCreateSerializer(serializers.ModelSerializer):
+    organization = serializers.PrimaryKeyRelatedField(required=True, queryset=Organization.objects.all())
+
+    class Meta:
+        model = Assistant
+        fields = ('id', 'organization', 'name', 'gender', 'position', 'image')
+
+    def validate_organization(self, organization):
+        if AssistantService.exists_for_organization(organization=organization):
+            raise serializers.ValidationError(_('Assistant already exists in this organization.'))
+        return organization
+
+
+class OrganizationAssistantAnswerCreateSerializer(serializers.ModelSerializer):
+    files = serializers.PrimaryKeyRelatedField(queryset=AnswerFile.objects.all(), many=True, required=False)
+
+    class Meta:
+        model = Answer
+        fields = ('id', 'assistant', 'question', 'text', 'files')
+
+    def create(self, validated_data):
+        files = validated_data.pop('files', [])
+        answer = Answer.objects.create(**validated_data)
+        answer.files.set(files)
+        return answer
+
+
+class OrganizationAssistantAnswerRetrieveSerializer(serializers.ModelSerializer):
+    files = AnswerFileSerializer(many=True)
+
+    class Meta:
+        model = Answer
+        fields = ('id', 'assistant', 'question', 'text', 'files')
+
+
+class OrganizationAssistantAnswerUpdateSerializer(serializers.ModelSerializer):
+    files = serializers.PrimaryKeyRelatedField(queryset=AnswerFile.objects.all(), many=True, required=False)
+
+    class Meta:
+        model = Answer
+        fields = ('id', 'assistant', 'question', 'text', 'files')
+
+    def update(self, instance, validated_data):
+        files_data = validated_data.pop('files', [])
+        instance.text = validated_data.get('text', instance.text)
+        if files_data is not None:
+            instance.files.clear()
+            for file_data in files_data:
+                answer_file = AnswerFile.objects.create(**file_data)
+                instance.files.add(answer_file)
+        instance.save()
+        return instance
+
+
+class QuestionListQueryParamSerializer(serializers.Serializer):
+    assistant_id = serializers.IntegerField(required=True)
+
+
+class QuestionListSerializer(serializers.ModelSerializer):
+    is_filled = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Question
+        fields = ['id', 'text', 'ordering', 'created_at', 'updated_at', 'is_filled']
+
+    def get_is_filled(self, question: Question):
+        assistant = self.context.get('assistant')
+        return Answer.objects.filter(question=question, assistant=assistant).exists()
+
+
+
+
+
+
+

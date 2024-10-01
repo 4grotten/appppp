92cec4ed192c084f06a837a951546daf3c3bafe8
Erzhan <erzhanmuratov99@gmail.com>
2024-10-01 16:01:07 +0600

    feat: add DeleteFCMDeviceDuplicatesAPIView

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import NotificationListAPIView, NotificationSettingAPIView, NotificationsCountAPIView
+from .views import NotificationListAPIView, NotificationSettingAPIView, NotificationsCountAPIView, \
+    DeleteFCMDeviceDuplicatesAPIView
 
 urlpatterns = [
     path('', NotificationListAPIView.as_view(), name='own_notifications'),
     path('settings/', NotificationSettingAPIView.as_view(), name='notification_setting'),
-    path('statistics/', NotificationsCountAPIView.as_view(), name='notification_count')
+    path('statistics/', NotificationsCountAPIView.as_view(), name='notification_count'),
+    path('delete-fcm-duplicates/', DeleteFCMDeviceDuplicatesAPIView.as_view(), name='delete_fcm_duplicates')
 ]

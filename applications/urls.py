4508b8ba5d4d78a354e00064b9a322218d334cd8
Erzhan <erzhanmuratov99@gmail.com>
2025-06-03 12:51:25 +0600

    feat: add List in UserAppListCreateView

--- applications/urls.py
+++ applications/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 
-from applications.views import UserAppCreateView, ToggleUserAppView
+from applications.views import UserAppListCreateView, ToggleUserAppView
 
 user_app_urls = [
-    path('applications/', UserAppCreateView.as_view(), name='user_applications'),
+    path('applications/', UserAppListCreateView.as_view(), name='user_applications'),
     path('applications/<int:pk>/toggle/', ToggleUserAppView.as_view(), name='toggle_user_app')
 ]
 urlpatterns = [

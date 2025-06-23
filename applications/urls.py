b4deca7b85fb75c7694befebb302addf57c21dcb
Erzhan <erzhanmuratov99@gmail.com>
2025-06-23 16:06:59 +0600

    feat: add slug in UserApp

--- applications/urls.py
+++ applications/urls.py
@@ -7,7 +7,7 @@ from applications.views import UserAppListCreateView, ToggleUserAppView, UserApp
 
 user_app_urls = [
     path('applications/', UserAppListCreateView.as_view(), name='user_applications'),
-    path('applications/<int:pk>/', UserAppRetrieveUpdateView.as_view(), name='user_application_detail'),
+    path('applications/<slug:slug>/', UserAppRetrieveUpdateView.as_view(), name='user_application_detail'),
     path('applications/<int:pk>/banners/', UserAppBannerListView.as_view(), name='user_application_banners'),
     path('applications/banners/<int:pk>/', RemoveUserAppCustomBannerView.as_view(),
          name='application-banners-delete'),

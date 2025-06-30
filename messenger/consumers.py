f34566203ff906e5caf17ff1df60419c7083d100
Savada <hahazov.ismar7@gmail.com>
2025-06-30 14:31:30 +0600

    revers

--- messenger/consumers.py
+++ messenger/consumers.py
@@ -71,8 +71,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
             parent=serializer.validated_data.get("parent")
         )
 
-        response_data = await sync_to_async(ChatMessageWSSerializer)(message, context={'user': user})
-        response_json = await sync_to_async(lambda s: s.data)(response_data)
+        serializer = ChatMessageWSSerializer(message, context={'user': user})
+        response_json = await sync_to_async(serializer.data.copy)()
 
         await self.channel_layer.group_send(
             self.room_group_name,

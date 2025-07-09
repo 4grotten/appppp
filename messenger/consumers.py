04add60356c9780f2980097e83c5fe239010e5c7
Savada <hahazov.ismar7@gmail.com>
2025-07-09 15:02:51 +0600

    asd

--- messenger/consumers.py
+++ messenger/consumers.py
@@ -175,7 +175,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 lambda: NotificationSetting.objects.filter(user=participant).first()
             )()
             logger.warning(
-                f"Notification setting for {participant}: {notification_setting}"
+                f"Notification setting for {await sync_to_async(participant)}: {await sync_to_async(notification_setting)}"
             )
             if not notification_setting:
                 continue

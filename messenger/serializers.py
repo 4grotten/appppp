1cc28615e8636b8bd29b64c109aee354f63782c7
Savada <hahazov.ismar7@gmail.com>
2025-07-14 18:30:03 +0600

    add users

--- messenger/serializers.py
+++ messenger/serializers.py
@@ -419,4 +419,6 @@ class OrganizationSimpleSerializer(serializers.ModelSerializer):
         request = self.context.get("request")
         if not request or not request.user.is_authenticated:
             return False
-        return organization.memberships.filter(user=request.user).exists()
+        return organization.objects.filter(
+            memberships__user=request.user, role__can_send_message=True
+        ).exists()

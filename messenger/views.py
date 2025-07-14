dd6502dc99c8a25f0e6c4ed5115dc89e07c74229
Savada <hahazov.ismar7@gmail.com>
2025-07-14 15:53:25 +0600

    changes

--- messenger/views.py
+++ messenger/views.py
@@ -914,9 +914,9 @@ class MessengerChatsOrganiationAPIView(APIView):
         exists_chat = MessengerChat.objects.filter(
             organization=organization,
             chat_type=GROUP,
-        ).first()
+        )
         if exists_chat:
-            memgers = exists_chat.values_list("members", flat=True)
+            memgers = exists_chat.first().values_list("members", flat=True)
             if user in memgers:
                 return Response(
                     {

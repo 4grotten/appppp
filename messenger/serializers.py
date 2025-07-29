d10446002b444d13ef14ef7a75f17812121f67cf
Savada <hahazov.ismar7@gmail.com>
2025-07-29 18:01:11 +0600

    add new field

--- messenger/serializers.py
+++ messenger/serializers.py
@@ -447,6 +447,8 @@ class OrganizationSimpleSerializer(serializers.ModelSerializer):
     types = OrganizationTypeSerializer(many=True)
     chat_id = serializers.SerializerMethodField()
     is_members = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
 
     class Meta:
         model = Organization
@@ -458,7 +460,23 @@ class OrganizationSimpleSerializer(serializers.ModelSerializer):
             "types",
             "chat_id",
             "is_members",
+            "updated_at",
+            "created_at",
         )
+
+    def get_updated_at(self, organization: Organization):
+        request = self.context.get("request")
+        if not request or not request.user.is_authenticated:
+            return None
+        chat = organization.messenger_chats.filter(members=request.user).first()
+        return chat.updated_at if chat else None
+
+    def get_created_at(self, organization: Organization):
+        request = self.context.get("request")
+        if not request or not request.user.is_authenticated:
+            return None
+        chat = organization.messenger_chats.filter(members=request.user).first()
+        return chat.created_at if chat else None
 
     def get_chat_id(self, organization: Organization):
         request = self.context.get("request")

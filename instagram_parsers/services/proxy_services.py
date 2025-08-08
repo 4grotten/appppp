61dcc776bf3814cd7a24f975b880d0c38de804ba
Savada <hahazov.ismar7@gmail.com>
2025-08-09 02:11:35 +0600

    add

--- instagram_parsers/services/proxy_services.py
+++ instagram_parsers/services/proxy_services.py
@@ -1,3 +1,4 @@
+import logging
 import random
 from typing import Optional
 
@@ -18,34 +19,50 @@ class InstagramClientService:
     def get_client(cls, for_getting_username: bool = False):
         login_device = LoginDeviceService.get_random_login_settings()
         # proxy = ProxyService.get_random_formed_proxy(for_getting_username=for_getting_username)
-        proxy = f'http://{login_device.proxy_login}:{login_device.proxy_password}@{login_device.proxy_http_s}'
+        proxy = f"http://{login_device.proxy_login}:{login_device.proxy_password}@{login_device.proxy_http_s}"
         return Client(settings=login_device.settings, proxy=proxy), login_device
 
     @classmethod
     def get_anon_client(cls, for_getting_username: bool = False) -> Client:
-        proxy = ProxyService.get_random_formed_proxy(for_getting_username=for_getting_username)
+        proxy = ProxyService.get_random_formed_proxy(
+            for_getting_username=for_getting_username
+        )
         return Client(proxy=proxy)
 
     @classmethod
     def get_random_proxy(cls, for_getting_username: bool = False):
-        proxy = ProxyService.get_random_formed_proxy(for_getting_username=for_getting_username)
+        proxy = ProxyService.get_random_formed_proxy(
+            for_getting_username=for_getting_username
+        )
         return proxy
+
 
 class ProxyService:
     @classmethod
-    def get_random_formed_proxy(cls, for_getting_username: bool = False) -> Optional[str]:
-        proxies = Proxy.objects.filter(for_getting_username=for_getting_username, expires_at__gt=now())
+    def get_random_formed_proxy(
+        cls, for_getting_username: bool = False
+    ) -> Optional[str]:
+        proxies = Proxy.objects.filter(
+            for_getting_username=for_getting_username, expires_at__gt=now()
+        )
+        logging.info(f"Proxies available: {proxies.count()}")
         if proxies:
+            proxies_info = [proxy.login for proxy in proxies]
+            logging.info(f"Available proxies: {proxies_info}")
             random_proxy = random.choice(proxies)
-            return f'http://{random_proxy.login}:{random_proxy.password}@{random_proxy.http_s}'
+            return f"http://{random_proxy.login}:{random_proxy.password}@{random_proxy.http_s}"
         return None
 
     @classmethod
     def get_random_proxy_for_requests(cls, for_getting_username: bool = False):
-        proxies = Proxy.objects.filter(for_getting_username=for_getting_username, expires_at__gt=now())
+        proxies = Proxy.objects.filter(
+            for_getting_username=for_getting_username, expires_at__gt=now()
+        )
         if proxies:
             proxy = random.choice(proxies)
-            list_proxies = [dict(https=f'http://{proxy.login}:{proxy.password}@{proxy.http_s}')]
+            list_proxies = [
+                dict(https=f"http://{proxy.login}:{proxy.password}@{proxy.http_s}")
+            ]
             return list_proxies
         return None
 
@@ -53,8 +70,10 @@ class ProxyService:
 class LoginDeviceService:
     @classmethod
     def get_random_login_settings(cls):
-        login_settings = LoginDevice.objects.filter(for_getting_username=True, is_broke=False)
+        login_settings = LoginDevice.objects.filter(
+            for_getting_username=True, is_broke=False
+        )
         if not login_settings:
-            raise Exception(_('Need at least one LoginDevice'))
+            raise Exception(_("Need at least one LoginDevice"))
         random_device = random.choice(login_settings)
         return random_device

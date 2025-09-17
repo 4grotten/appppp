9aa4c3e3e12e56fe19b6302ec01d4f67fd6d4bdc
Islam <kirolkuro@gmail.com>
2025-09-17 15:56:58 +0600

    fix

--- notifications/views.py
+++ notifications/views.py
@@ -10,9 +10,17 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .filters import NotificationFilter
-from .serializers import (NotificationSerializer, CustomFCMDeviceSerializer, NotificationSettingSerializer,
-                          FCMDeviceSettingsSerializer)
-from .services import NotificationService, NotificationSettingService, FCMDeviceSettingsService
+from .serializers import (
+    NotificationSerializer,
+    CustomFCMDeviceSerializer,
+    NotificationSettingSerializer,
+    FCMDeviceSettingsSerializer,
+)
+from .services import (
+    NotificationService,
+    NotificationSettingService,
+    FCMDeviceSettingsService,
+)
 
 
 class NotificationListAPIView(ListAPIView):
@@ -34,7 +42,9 @@ class NotificationSettingAPIView(APIView):
     serializer_class = NotificationSettingSerializer
 
     def get(self, request):
-        notification_setting = NotificationSettingService.get_or_create(user=request.user)
+        notification_setting = NotificationSettingService.get_or_create(
+            user=request.user
+        )
 
         return Response(self.serializer_class(notification_setting, many=False).data)
 
@@ -42,35 +52,40 @@ class NotificationSettingAPIView(APIView):
         serializer = self.serializer_class(data=request.data)
 
         if not serializer.is_valid():
-            return Response(data={
-                'message': _('Invalid input'),
-                'errors': serializer.errors
-            }, status=status.HTTP_406_NOT_ACCEPTABLE)
+            return Response(
+                data={"message": _("Invalid input"), "errors": serializer.errors},
+                status=status.HTTP_406_NOT_ACCEPTABLE,
+            )
 
-        notification_setting = NotificationSettingService.get_or_create(user=request.user)
-
-        updated_notification_setting = NotificationSettingService.update(
-            notification_setting=notification_setting,
-            **serializer.validated_data
+        notification_setting = NotificationSettingService.get_or_create(
+            user=request.user
         )
 
-        return Response(self.serializer_class(updated_notification_setting, many=False).data)
+        updated_notification_setting = NotificationSettingService.update(
+            notification_setting=notification_setting, **serializer.validated_data
+        )
+
+        return Response(
+            self.serializer_class(updated_notification_setting, many=False).data
+        )
 
 
 class NotificationsCountAPIView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
-        return Response(data={
-            'count': NotificationService.get_user_notifications_count(user=request.user)
-        })
+        return Response(
+            data={
+                "count": NotificationService.get_user_notifications_count(
+                    user=request.user
+                )
+            }
+        )
 
     def post(self, request):
         NotificationService.do_read_notifications(user=request.user)
 
-        return Response(data={
-            'message': _('Success')
-        })
+        return Response(data={"message": _("Success")})
 
 
 class FCMDeviceSettingsAPIView(APIView):
@@ -81,15 +96,13 @@ class FCMDeviceSettingsAPIView(APIView):
         serializer = self.serializer_class(data=request.data)
 
         if not serializer.is_valid():
-            return Response(data={
-                'message': _('Invalid input'),
-                'errors': serializer.errors
-            }, status=status.HTTP_406_NOT_ACCEPTABLE)
+            return Response(
+                data={"message": _("Invalid input"), "errors": serializer.errors},
+                status=status.HTTP_406_NOT_ACCEPTABLE,
+            )
         FCMDeviceSettingsService.create(**serializer.validated_data, user=request.user)
 
-        return Response(data={
-            'message': _('Success')
-        })
+        return Response(data={"message": _("Success")})
 
 
 class DeleteFCMDeviceDuplicatesAPIView(APIView):
@@ -100,16 +113,21 @@ class DeleteFCMDeviceDuplicatesAPIView(APIView):
     def post(self, request):
         try:
             # Step 1: Group FCMDevices by registration_id and find duplicates
-            devices = FCMDevice.objects.values('registration_id').annotate(duplicate_count=Count('id')).filter(
-                duplicate_count__gt=1)
+            devices = (
+                FCMDevice.objects.values("registration_id")
+                .annotate(duplicate_count=Count("id"))
+                .filter(duplicate_count__gt=1)
+            )
 
             deleted_count = 0
 
             for device_group in devices:
-                reg_id = device_group['registration_id']
+                reg_id = device_group["registration_id"]
 
                 # Step 2: Find all devices with the same registration_id
-                duplicates = FCMDevice.objects.filter(registration_id=reg_id)
+                duplicates = FCMDevice.objects.filter(registration_id=reg_id).order_by(
+                    "-created_at"
+                )
 
                 # Step 3: Keep the first device and delete the others
                 if duplicates.count() > 1:
@@ -122,12 +140,13 @@ class DeleteFCMDeviceDuplicatesAPIView(APIView):
                         deleted_count += 1
 
             return Response(
-                {"message": f"Successfully deleted {deleted_count} duplicate FCMDevice entries."},
-                status=status.HTTP_200_OK
+                {
+                    "message": f"Successfully deleted {deleted_count} duplicate FCMDevice entries."
+                },
+                status=status.HTTP_200_OK,
             )
 
         except Exception as e:
             return Response(
-                {"error": str(e)},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )

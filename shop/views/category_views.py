f9cdeb9f8b7545a17b1d90c04eef5f32e5f1d208
Islam <kirolkuro@gmail.com>
2025-09-30 20:39:11 +0600

    fix

--- shop/views/category_views.py
+++ shop/views/category_views.py
@@ -243,8 +243,14 @@ class ItemSubcategoryCreateView(CreateAPIView):
         name_zh = GoogleTranslator().translate(
             serializer.validated_data["name"], "zh-CN"
         )
+        name = serializer.validated_data["name"]
         if ItemSubcategory.objects.filter(
-            name=serializer.validated_data["name"]
+            Q(name=name)
+            | Q(name_ru=name)
+            | Q(name_en=name)
+            | Q(name_tr=name)
+            | Q(name_de=name)
+            | Q(name_zh=name)
         ).exists():
             raise SubcategoryExist()
         subcategory = ItemSubcategory.objects.create(

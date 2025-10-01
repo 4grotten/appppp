263ae63eb07f0a511ceaa58e9c7aba5ffcf91861
Islam <kirolkuro@gmail.com>
2025-10-01 18:09:31 +0600

    fix

--- organizations/views/organization_tariff.py
+++ organizations/views/organization_tariff.py
@@ -2,11 +2,15 @@ from rest_framework import generics
 from rest_framework.response import Response
 from organizations.services.invoice_service import OrganizationInvoiceService
 from organizations.serializers.invoice_serializers import InvoiceCreateSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class OrganizationTariffInvoiceAPIView(generics.GenericAPIView):
     serializer_class = InvoiceCreateSerializer
     service_class = OrganizationInvoiceService
+    permission_classes = [
+        IsAuthenticated,
+    ]
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)

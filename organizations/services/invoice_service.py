c732fc2cd1040811e483524f890042ccab4a7823
Islam <kirolkuro@gmail.com>
2025-10-31 15:25:59 +0600

    fix

--- organizations/services/invoice_service.py
+++ organizations/services/invoice_service.py
@@ -151,7 +151,11 @@ class OrganizationInvoiceService:
 
     @classmethod
     def get_invoice_by_invoice_number(cls, invoice_number: str) -> Union[Invoice, None]:
-        qs = Invoice.objects.filter(invoice_number=invoice_number).first()
+        qs = (
+            Invoice.objects.filter(invoice_number=invoice_number)
+            .select_related("user", "organization_info", "subscription", "tariff")
+            .first()
+        )
         print(qs)
 
         return qs

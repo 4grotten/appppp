5542bc34a155fcf0685bcbfeb724dcc0fc30cfd7
Islam <kirolkuro@gmail.com>
2025-11-05 23:44:13 +0600

    fix

--- organizations/admin.py
+++ organizations/admin.py
@@ -918,11 +918,18 @@ class UserOrgSubscriptionAdmin(admin.ModelAdmin):
 @admin.register(Invoice)
 class InvoiceAdmin(admin.ModelAdmin):
     change_form_template = "admin/invoice_change_form.html"
+    readonly_fields = ["organization_info"]
 
     list_display = [
         "invoice_number",
         "invoice_pdf",
         "receipt_pdf",
+    ]
+
+    search_fields = [
+        "invoice_number",
+        "organization_info__organization__title",
+        "organization_info__organization__id",
     ]
 
     def get_urls(self):

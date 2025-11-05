d7e40d3895cebfea724d09e6c1fb263dd3227041
Islam <kirolkuro@gmail.com>
2025-11-05 22:25:12 +0600

    fix

--- organizations/services/invoice_service.py
+++ organizations/services/invoice_service.py
@@ -241,8 +241,8 @@ class OrganizationInvoiceService:
 
         qs = Invoice.objects.filter(
             organization_info__organization_id=organization_id,
-            receipt_pdf__isnull=False,
-        ).exclude(receipt_pdf="")
+            receipt_pdf="",
+        )
 
         return qs
 

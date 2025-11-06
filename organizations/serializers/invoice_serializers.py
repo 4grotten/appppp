4d5870d89edb984a38c4d4854f048496ee4f4d38
Islam <kirolkuro@gmail.com>
2025-11-06 21:35:48 +0600

    fix

--- organizations/serializers/invoice_serializers.py
+++ organizations/serializers/invoice_serializers.py
@@ -114,6 +114,7 @@ class ReceiptListSerializer(serializers.Serializer):
     def to_representation(self, instance):
         url = create_download_url(instance.invoice_pdf)
         data = super().to_representation(instance)
+        data["tax_amount"] = CountryInvoiceInfo.objects.get(country=data["code"]).tax
         data["receipt_download"] = url
 
         return data

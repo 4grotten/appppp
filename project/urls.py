fa6238e7362c5ac6673645ccc53e4c30a900729d
LangaMad <artykovh@gmail.com>
2025-12-27 19:40:08 +0600

    add swagger

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import permissions
 from django.http import HttpResponseRedirect
 
@@ -56,6 +56,29 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    try:
+        from drf_yasg.views import get_schema_view
+        from drf_yasg import openapi
+
+        schema_view = get_schema_view(
+            openapi.Info(
+                title="API Documentation",
+                default_version='v1',
+                description="Документация доступна только в режиме отладки",
+            ),
+            public=True,
+            permission_classes=(permissions.AllowAny,),
+        )
+
+        urlpatterns += [
+            re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+            path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+            path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+        ]
+    except ImportError:
+
+        pass
 
 if settings.MONITORING:
     urlpatterns += (path("", include("django_prometheus.urls")),)

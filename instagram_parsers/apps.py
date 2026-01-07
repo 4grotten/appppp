20cb07b4e60a804c25a7f43735819b2b06ef8c92
LangaMad <artykovh@gmail.com>
2026-01-07 16:59:08 +0600

    add signals for share hikerApi from models

--- instagram_parsers/apps.py
+++ instagram_parsers/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class InstagramParsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'instagram_parsers'
+
+    def ready(self):
+        import instagram_parsers.signals

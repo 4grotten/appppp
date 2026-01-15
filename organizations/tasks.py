880abd0241ca00d989399bac3c1ffeff0b155b67
LangaMad <artykovh@gmail.com>
2026-01-15 17:09:03 +0600

    add org_url for ai

--- organizations/tasks.py
+++ organizations/tasks.py
@@ -280,7 +280,7 @@ def process_comment_with_assistant(
     comment = Comment.objects.get(id=comment_id)
     assistant = Assistant.objects.get(id=assistant_id)
     catalog_url = AssistantDataService.get_file_url(assistant.organization)
-
+    org_url = f"{settings.SITE_URL}/organizations/{assistant.organization.id}"
     try:
         response = requests.post(
             "http://161.35.153.151:8080/bot/comments/",
@@ -296,6 +296,7 @@ def process_comment_with_assistant(
                     },
                     "item_info": item_info,
                     "organization_info": organization_info,
+                    "organization_page_url": org_url,
                     "catalog_file": catalog_url
                 },
             },

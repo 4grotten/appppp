51e48dd6ee22dd1bdf3cfc773eb64eeb9e9fa571
askat <raven@askats-MacBook-Air.local>
2026-03-16 16:27:33 +0600

    external

--- organizations/migrations/0197_regionalpaymentsystemsettings_conversion_fee_percent.py
+++ organizations/migrations/0197_regionalpaymentsystemsettings_conversion_fee_percent.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('organizations', '0196_auto_20260313_2210'),
+        ('organizations', '0196_assistant_external_api_methods'),
     ]
 
     operations = [
